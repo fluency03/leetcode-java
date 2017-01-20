6f1bdbd738cf6e4d97f576f9d27123fb1d9a95d9
fluency03 <fluency.03@gmail.com>
2017-01-20 21:13:24 +0100

    GenerateParentheses22.java

---
+++ GenerateParentheses22.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class GenerateParentheses22 {
+    public List<String> generateParenthesis(int n) {
+        if (n <= 0) {
+            return new ArrayList<String>();
+        } else if (n == 1) {
+            List<String> temp = new ArrayList<String>();
+            temp.add("()");
+            return temp;
+        } else {
+            return addOnePair(generateParenthesis(n-1), n);
+        }
+
+    }
+
+    private List<String> addOnePair(List<String> source, Integer n) {
+        Set<String> returned = new HashSet<String>();
+        for (String prev: source) {
+            for (int i=0; i<n; i++) {
+                returned.add(prev.substring(0, n-1+i) + "()" + prev.substring(n-1+i, 2*n-2));
+            }
+        }
+        return new ArrayList(returned);
+    }
+
+    /* ------------------------------------------------------------------- */
+    
+
+    public List<String> generateParenthesis2(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+      GenerateParentheses22 gp = new GenerateParentheses22();
+      System.out.println(gp.generateParenthesis(3));
+      System.out.println(gp.generateParenthesis2(3));
+    }
+
+}
