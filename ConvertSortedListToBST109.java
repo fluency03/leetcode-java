054c870d88a9d6ecbb4af5d19962b46ac63a5c11
fluency03 <fluency.03@gmail.com>
2017-01-22 13:08:59 +0100

    ConvertSortedListToBST109.java

--- ConvertSortedListToBST109.java
+++ ConvertSortedListToBST109.java
@@ -53,10 +53,75 @@ public class ConvertSortedListToBST109 {
         return node;
     }
 
+    /** -------------------------------------------------------------------
+     * Top Solution: O(n logn)
+     * https://discuss.leetcode.com/topic/35997/share-my-java-solution-1ms-very-short-and-concise
+     * --------------------------------------------------------------------
+     */
 
-    public static void main(String[] args) {
-        ConvertSortedListToBST109 sl2bst = new ConvertSortedListToBST109();
-
+    public TreeNode sortedListToBST2(ListNode head) {
+        if (head == null) return null;
+        return toBST(head, null);
     }
+
+    public TreeNode toBST(ListNode head, ListNode tail){
+        ListNode slow = head;
+        ListNode fast = head;
+        if(head == tail) return null;
+
+        while(fast != tail && fast.next != tail){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode thead = new TreeNode(slow.val);
+        thead.left = toBST(head, slow);
+        thead.right = toBST(slow.next, tail);
+        return thead;
+    }
+
+
+    /** -------------------------------------------------------------------
+     * Top Solution: O(n)
+     * https://discuss.leetcode.com/topic/35997/share-my-java-solution-1ms-very-short-and-concise
+     * --------------------------------------------------------------------
+     */
+
+    private ListNode node;
+
+    public TreeNode sortedListToBST3(ListNode head) {
+      	if(head == null){
+      		  return null;
+      	}
+
+      	int size = 0;
+      	ListNode runner = head;
+      	node = head;
+
+      	while(runner != null){
+      		  runner = runner.next;
+      		      size ++;
+      	}
+
+      	return inorderHelper(0, size - 1);
+    }
+
+    public TreeNode inorderHelper(int start, int end){
+    	  if(start > end){
+    		    return null;
+    	  }
+
+        int mid = start + (end - start) / 2;
+        TreeNode left = inorderHelper(start, mid - 1);
+
+        TreeNode treenode = new TreeNode(node.val);
+        treenode.left = left;
+        node = node.next;
+
+        TreeNode right = inorderHelper(mid + 1, end);
+        treenode.right = right;
+
+        return treenode;
+    }
+
 
 }
