72844942f80da9ae999e7a2beabd43fc5464fe45
fluency03 <fluency.03@gmail.com>
2017-03-06 10:26:19 +0100

    Interval

---
+++ Interval.java
@@ -0,0 +1,10 @@
+/**
+ * Definition for an interval.
+ */
+
+public class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
