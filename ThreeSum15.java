d3142c334e453fcf46edf8a94f44c4380e33f670
fluency03 <fluency.03@gmail.com>
2017-05-21 13:47:44 +0200

    refactor

--- ThreeSum15.java
+++ ThreeSum15.java
@@ -16,28 +16,42 @@
 
 
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+
 
 
 public class ThreeSum15 {
 
-    public List<List<Integer>> threeSum(int[] num) {
-        Arrays.sort(num);
+    /**
+     * https://discuss.leetcode.com/topic/8125/concise-o-n-2-java-solution
+     */
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> res = new LinkedList<>();
-        for (int i = 0; i < num.length-2; i++) {
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {
-                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                        while (lo < hi && num[lo] == num[lo+1]) lo++;
-                        while (lo < hi && num[hi] == num[hi-1]) hi--;
-                        lo++; hi--;
-                    } else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int low = i + 1;
+                int high = nums.length - 1;
+                int sum = 0 - nums[i];
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while (low < high && nums[low] == nums[low + 1]) {
+                          low++;
+                        }
+                        while (low < high && nums[high] == nums[high - 1]) {
+                          high--;
+                        }
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] < sum) {
+                        low++;
+                    }
+                    else {
+                        high--;
+                    }
                }
             }
         }
