8028caaa11d4a0b6ddb6b7fa228ff3f19494eb6f
fluency03 <fluency.03@gmail.com>
2017-05-21 13:49:15 +0200

    refactor

--- ThreeSum15.java
+++ ThreeSum15.java
@@ -29,7 +29,9 @@ public class ThreeSum15 {
 
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new LinkedList<>();
+
+        List<List<Integer>> returned = new LinkedList<>();
+
         for (int i = 0; i < nums.length - 2; i++) {
             if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
                 int low = i + 1;
@@ -37,7 +39,7 @@ public class ThreeSum15 {
                 int sum = 0 - nums[i];
                 while (low < high) {
                     if (nums[low] + nums[high] == sum) {
-                        res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        returned.add(Arrays.asList(nums[i], nums[low], nums[high]));
                         while (low < high && nums[low] == nums[low + 1]) {
                           low++;
                         }
@@ -55,7 +57,7 @@ public class ThreeSum15 {
                }
             }
         }
-        return res;
+        return returned;
     }
 
     public static void main(String[] args) {
