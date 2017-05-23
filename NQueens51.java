5a452707a87f2a2e7cfed6f6e038ed32f011d526
fluency03 <fluency.03@gmail.com>
2017-05-23 23:44:27 +0200

    51 other solution

--- NQueens51.java
+++ NQueens51.java
@@ -159,6 +159,34 @@ public class NQueens51 {
     }
 
 
+    /**
+     * https://discuss.leetcode.com/topic/8592/comparably-concise-java-code
+     */
+    private void helper(int r, boolean[] cols, boolean[] d1, boolean[] d2, String[] board, List<List<String>> res) {
+        if (r == board.length) {
+            res.add(Arrays.asList(board.clone()));
+        } else {
+            for (int c = 0; c < board.length; c++) {
+                int id1 = r - c + board.length, id2 = 2*board.length - r - c - 1;
+                if (!cols[c] && !d1[id1] && !d2[id2]) {
+                    char[] row = new char[board.length];
+                    Arrays.fill(row, '.'); row[c] = 'Q';
+                    board[r] = new String(row);
+                    cols[c] = true; d1[id1] = true; d2[id2] = true;
+                    helper(r+1, cols, d1, d2, board, res);
+                    cols[c] = false; d1[id1] = false; d2[id2] = false;
+                }
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        helper(0, new boolean[n], new boolean[2*n], new boolean[2*n], new String[n], res);
+        return res;
+    }
+
+
     public static void main(String[] args) {
         NQueens51 nq = new NQueens51();
 
