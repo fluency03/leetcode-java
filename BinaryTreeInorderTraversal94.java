2ae19879af443bf07e6a787a400c60503129a055
fluency03 <fluency.03@gmail.com>
2017-05-26 16:53:53 +0200

    94

---
+++ BinaryTreeInorderTraversal94.java
@@ -0,0 +1,75 @@
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * For example:
+ * Given binary tree [1,null,2,3],
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * return [1,3,2].
+ *
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+public class BinaryTreeInorderTraversal94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (node == null) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> result =  new ArrayList<>();
+
+        if (root.left == null && root.right == null) {
+            result.add(root.val);
+            return result;
+        }
+
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+
+        return result;
+    }
+
+    /**
+     * https://discuss.leetcode.com/topic/6478/iterative-solution-in-java-simple-and-readable
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while (cur != null || !stack.empty()){
+            while (cur != null){
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+
+        return list;
+    }
+
+}
