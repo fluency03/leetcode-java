6ebdf3f554c4317c15d6a1b137f89fae045a4642
fluency03 <fluency.03@gmail.com>
2017-05-29 23:42:20 +0200

    SearchInsertPosition35

---
+++ src/SearchInsertPosition35.java
@@ -0,0 +1,37 @@
+/**
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ *
+ * You may assume no duplicates in the array.
+ *
+ * Here are few examples.
+ * [1,3,5,6], 5 → 2
+ * [1,3,5,6], 2 → 1
+ * [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ */
+
+
+public class SearchInsertPosition35 {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+
+    public int searchInsert2(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+}
