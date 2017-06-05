a911f2bfb7efd907ca94079704e60c91a6882539
fluency03 <fluency.03@gmail.com>
2017-06-05 14:58:39 +0200

    Trie

---
+++ src/Trie.java
@@ -0,0 +1,67 @@
+/**
+ * 208. Implement Trie (Prefix Tree)
+ *
+ * Implement a trie with insert, search, and startsWith methods.
+ *
+ * Note:
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ */
+
+
+
+public class Trie {
+
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!node.containsKey(c)) {
+                node.put(c, new TrieNode());
+            }
+            node = node.get(c);
+        }
+        node.setLeaf();
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!node.containsKey(c)) {
+                return false;
+            }
+            node = node.get(c);
+        }
+        return node.isLeaf();
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (!node.containsKey(c)) {
+                return false;
+            }
+            node = node.get(c);
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
