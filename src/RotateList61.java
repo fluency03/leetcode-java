c4b33917d7cc58941cee9dd27764de8aaea2edb1
fluency03 <fluency.03@gmail.com>
2017-06-26 21:17:05 +0200

    RotateList61

--- src/RotateList61.java
+++ src/RotateList61.java
@@ -46,6 +46,55 @@ public class RotateList61 {
     }
 
 
+    /**
+     * https://discuss.leetcode.com/topic/2861/share-my-java-solution-with-explanation
+     */
+    public ListNode rotateRight2(ListNode head, int n) {
+        if (head==null||head.next==null) return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode fast=dummy,slow=dummy;
 
+        int i;
+        for (i=0;fast.next!=null;i++)//Get the total length
+        	fast=fast.next;
+
+        for (int j=i-n%i;j>0;j--) //Get the i-n%i th node
+        	slow=slow.next;
+
+        fast.next=dummy.next; //Do the rotation
+        dummy.next=slow.next;
+        slow.next=null;
+
+        return dummy.next;
+    }
+
+
+    /**
+     * https://discuss.leetcode.com/topic/26364/clean-java-solution-with-brief-explanation
+     */
+    public ListNode rotateRight3(ListNode head, int k) {
+        if (head == null)
+        	return head;
+
+      	ListNode copyHead = head;
+
+      	int len = 1;
+      	while (copyHead.next != null) {
+      		copyHead = copyHead.next;
+      		len++;
+      	}
+
+      	copyHead.next = head;
+
+      	for (int i = len - k % len; i > 1; i--)
+      		head = head.next;
+
+      	copyHead = head.next;
+      	head.next = null;
+
+      	return copyHead;
+
+    }
 
 }
