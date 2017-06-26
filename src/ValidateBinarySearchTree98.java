ae2e931b6b637ab8492bc0816e448c2f7867660c
fluency03 <fluency.03@gmail.com>
2017-06-26 19:50:27 +0200

    isValidBST

--- src/ValidateBinarySearchTree98.java
+++ src/ValidateBinarySearchTree98.java
@@ -36,20 +36,37 @@ public class ValidateBinarySearchTree98 {
         return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private boolean helper(TreeNode root, Long min, Long max) {
+    private boolean helper(TreeNode root, long min, long max) {
         if (root == null) return true;
 
         if (root.val <= min || root.val >= max) {
             return false;
         }
 
-        return helper(root.left, min, Math.min(root.val, max)) && helper(root.right, Math.max(root.val, min), max);
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
     }
 
 
 
-
-
+    /**
+     * https://discuss.leetcode.com/topic/46016/learn-one-iterative-inorder-traversal-apply-it-to-multiple-tree-questions-java-solution
+     */
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
 
 
 
