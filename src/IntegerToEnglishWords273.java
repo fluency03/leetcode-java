031f5d523700575dfe07658f09ae5ea400f27c43
fluency03 <fluency.03@gmail.com>
2017-07-02 23:25:27 +0200

    IntegerToEnglishWords273

--- src/IntegerToEnglishWords273.java
+++ src/IntegerToEnglishWords273.java
@@ -15,43 +15,30 @@ public class IntegerToEnglishWords273 {
         "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
         "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
     private static final String[] TENS = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private static final String[] DOTS = {"Thousand", "Million", "Billion"};
+    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
 
 
     public String numberToWords(int num) {
         if (num == 0) return "Zero";
 
-        if (num < Math.pow(10, 3)) {
-            return threeDigits(num).trim();
-        } else if (num < Math.pow(10, 6)) {
-            String thou = threeDigits(num/1000);
-            String hund = threeDigits(num%1000);
-            return (thou + " Thousand" +
-                    ((hund.trim().length() == 0) ? "": hund)).trim();
-        } else if (num < Math.pow(10, 9)) {
-            String mill = threeDigits(num/1000000);
-            String thou = threeDigits((num/1000)%1000);
-            String hund = threeDigits(num%1000);
-            return (mill + " Million" +
-                    ((thou.trim().length() == 0) ? "" : thou + " Thousand") +
-                    ((hund.trim().length() == 0) ? "": hund)).trim();
-        } else {
-            String bill = threeDigits(num/1000000000);
-            String mill = threeDigits((num/1000000)%1000);
-            String thou = threeDigits((num/1000)%1000);
-            String hund = threeDigits(num%1000);
-            return (bill + " Billion" +
-                    ((mill.trim().length() == 0) ? "" : mill + " Million") +
-                    ((thou.trim().length() == 0) ? "" : thou + " Thousand") +
-                    ((hund.trim().length() == 0) ? "" : hund)).trim();
+        int i = 0;
+        String words = "";
+        while (num > 0) {
+            if (num % 1000 != 0) {
+        	    words = threeDigits(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+        	num /= 1000;
+        	i++;
         }
+
+        return words.trim();
     }
 
 
     private String threeDigits(int num) {
         if (num >= 100) {
             int h = num/100;
-            return " " + UNITS[h] + " Hundred" + twoDigits(num%100);
+            return UNITS[h] + " Hundred " + twoDigits(num%100);
         }
         return twoDigits(num);
     }
@@ -60,13 +47,13 @@ public class IntegerToEnglishWords273 {
         if (num == 0) return "";
 
         if (num > 0 && num < 20) {
-            return " " + UNITS[num];
+            return UNITS[num] + " ";
         }
 
         int t = num/10;
         int n = num%10;
 
-        return " " + TENS[t-2] + ((n == 0) ? "": (" " + UNITS[n]));
+        return TENS[t-2] + ((n == 0) ? "": (" " + UNITS[n])) + " ";
     }
 
 
