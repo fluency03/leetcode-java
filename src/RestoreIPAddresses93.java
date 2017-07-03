567bd4e7f38870eb6f0f67ab6960714a12737a63
fluency03 <fluency.03@gmail.com>
2017-07-03 12:09:53 +0200

    restoreIpAddresses2

--- src/RestoreIPAddresses93.java
+++ src/RestoreIPAddresses93.java
@@ -48,11 +48,35 @@ public class RestoreIPAddresses93 {
 
     private boolean isValid(String current) {
         // starts with "0" but not "0"
-        if (current.startsWith("0") && current.length() > 1) return false;
+        if (current.charAt(0)=='0' && current.length() > 1) return false;
         // larger than 255
         if (Integer.valueOf(current) > 255) return false;
         return true;
     }
 
+
+    /**
+     * https://discuss.leetcode.com/topic/3919/my-code-in-java
+     */
+    public List<String> restoreIpAddresses2(String s) {
+        List<String> res = new ArrayList<String>();
+        int len = s.length();
+        for(int i = 1; i<4 && i<len-2; i++){
+            for(int j = i+1; j<i+4 && j<len-1; j++){
+                for(int k = j+1; k<j+4 && k<len; k++){
+                    String s1 = s.substring(0,i), s2 = s.substring(i,j), s3 = s.substring(j,k), s4 = s.substring(k,len);
+                    if(isValid2(s1) && isValid2(s2) && isValid2(s3) && isValid2(s4)){
+                        res.add(s1+"."+s2+"."+s3+"."+s4);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    public boolean isValid2(String s){
+        if(s.length()>3 || s.length()==0 || (s.charAt(0)=='0' && s.length()>1) || Integer.parseInt(s)>255)
+            return false;
+        return true;
+    }
 
 }
