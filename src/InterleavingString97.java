33bc4faf0a50eb33e191e3fea849b42ddd9a2cb6
fluency03 <fluency.03@gmail.com>
2017-07-03 15:43:13 +0200

    isInterleave3 DP, stop early

--- src/InterleavingString97.java
+++ src/InterleavingString97.java
@@ -68,7 +68,7 @@ public class InterleavingString97 {
     }
 
     // DP
-    public boolean isInterleave(String s1, String s2, String s3) {
+    public boolean isInterleave2(String s1, String s2, String s3) {
         int l1 = s1.length();
         int l2 = s2.length();
         int l3 = s3.length();
@@ -98,5 +98,64 @@ public class InterleavingString97 {
         return dp[l2][l1];
     }
 
+    // DP, stop early
+    public boolean isInterleave3(String s1, String s2, String s3) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int l3 = s3.length();
+        if ((l1 + l2) != l3) return false;
+
+        boolean[][] dp = new boolean[l2+1][l1+1];
+        dp[0][0] = true;
+
+        int i = 1;
+        int j = 1;
+        for (i = 1; i<=l2; i++) {
+            dp[i][0] = dp[i-1][0] && (s3.charAt(i-1) == s2.charAt(i-1));
+        }
+
+        boolean l = false;
+        boolean one = false;
+        for (j = 1; j<=l1; j++) {
+            dp[0][j] = dp[0][j-1] && (s3.charAt(j-1) == s1.charAt(j-1));
+            one = dp[0][j] || one;
+        }
+        l = one;
+
+        for (i=1; i<=l2; i++) {
+            if (dp[i][0] == false && l == false) {
+                return false;
+            }
+            one = false;
+            for (j=1; j<=l1; j++) {
+                dp[i][j] = (dp[i-1][j] && s3.charAt(i+j-1) == s2.charAt(i-1)) || (dp[i][j-1] && s3.charAt(i+j-1) == s1.charAt(j-1));
+                one = dp[i][j] || one;
+            }
+            l = one;
+        }
+
+        return dp[l2][l1];
+    }
+
+
+    /**
+     * https://discuss.leetcode.com/topic/31991/1ms-tiny-dfs-beats-94-57
+     */
+    public boolean isInterleave4(String s1, String s2, String s3) {
+        char[] c1 = s1.toCharArray(), c2 = s2.toCharArray(), c3 = s3.toCharArray();
+    	int m = s1.length(), n = s2.length();
+    	if(m + n != s3.length()) return false;
+    	return dfs(c1, c2, c3, 0, 0, 0, new boolean[m + 1][n + 1]);
+    }
+
+    public boolean dfs(char[] c1, char[] c2, char[] c3, int i, int j, int k, boolean[][] invalid) {
+    	if(invalid[i][j]) return false;
+    	if(k == c3.length) return true;
+    	boolean valid =
+    	    i < c1.length && c1[i] == c3[k] && dfs(c1, c2, c3, i + 1, j, k + 1, invalid) ||
+            j < c2.length && c2[j] == c3[k] && dfs(c1, c2, c3, i, j + 1, k + 1, invalid);
+    	if(!valid) invalid[i][j] = true;
+        return valid;
+    }
 
 }
