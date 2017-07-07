8748c81d1aa1b85f33a022c885ac9f8b0f0a6b45
fluency03 <fluency.03@gmail.com>
2017-07-07 15:19:56 +0200

    WiggleSortII324

--- src/WiggleSortII324.java
+++ src/WiggleSortII324.java
@@ -12,52 +12,71 @@
  * Can you do it in O(n) time and/or in-place with O(1) extra space?
  */
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.ArrayList;
-import java.util.List;
 
 public class WiggleSortII324 {
-    // too slow
+    /**
+     * https://www.hrwhisper.me/leetcode-wiggle-sort-ii/
+     */
     public void wiggleSort(int[] nums) {
-        int l = nums.length;
-
         Arrays.sort(nums);
-        LinkedList<Integer> numsLL = new LinkedList<Integer>();
-        for (int i = 0; i < l; i++) {
-            numsLL.add(nums[i]);
-        }
-        LinkedList<Integer> sorted = new LinkedList();
-
-        int last = 0;
-        if (l%2 == 1) {
-            last = numsLL.get(l/2);
-            numsLL.remove(l/2);
+        int[] temp = new int[nums.length];
+        int s = (nums.length + 1) >> 1, t = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            temp[i] = (i & 1) == 0 ?  nums[--s] : nums[--t] ;
         }
 
-        int mid = numsLL.size()/2 - 1;
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = temp[i];
+    }
 
-        while (mid >= 0) {
-            Integer first = numsLL.get(mid);
-            sorted.add(first);
-            Integer next = mid + 1;
-            while (numsLL.get(next).equals(first)) {
-                next++;
-            }
-            sorted.add(numsLL.get(next));
-            numsLL.remove(mid);
-            numsLL.remove(next - 1);
-            mid--;
+
+    /**
+     * https://www.hrwhisper.me/leetcode-wiggle-sort-ii/
+     */
+    public void wiggleSort2(int[] nums) {
+        int medium = findMedium(nums, 0, nums.length - 1, (nums.length + 1) >> 1);
+        int s = 0, t = nums.length - 1 , mid_index = (nums.length + 1) >> 1;
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < medium)
+                temp[s++] = nums[i];
+            else if (nums[i] > medium)
+                temp[t--] = nums[i];
         }
 
-        if (l%2 == 1) {
-            sorted.add(last);
-        }
+        while (s < mid_index) temp[s++] = medium;
+        while (t >= mid_index) temp[t--] = medium;
 
-        for (int i = 0; i < l; i++) {
-            nums[i] = sorted.get(i);
-        }
+        t = nums.length;
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = (i & 1) == 0 ? temp[--s] : temp[--t];
+    }
 
+    private int findMedium(int[] nums, int L, int R, int k) {
+        if (L >= R) return nums[R];
+        int i = partition(nums, L, R);
+        int cnt = i - L + 1;
+        if (cnt == k) return nums[i];
+        return cnt > k ? findMedium(nums, L, i - 1, k) : findMedium(nums, i + 1, R, k - cnt);
+    }
+
+    private int partition(int[] nums, int L, int R) {
+        int val = nums[L];
+        int i = L, j = R + 1;
+        while (true) {
+            while (++i < R && nums[i] < val) ;
+            while (--j > L && nums[j] > val) ;
+            if (i >= j) break;
+            swap(nums, i, j);
+        }
+        swap(nums, L, j);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
 
@@ -65,13 +84,4 @@ public class WiggleSortII324 {
 
 
 
-
-
-
-    public static void main(String[] args) {
-        WiggleSortII324 ws2 = new WiggleSortII324();
-
-        ws2.wiggleSort(new int[]{1, 5, 1, 1, 6, 4});
-        ws2.wiggleSort(new int[]{1, 3, 2, 2, 3, 1});
-    }
 }
