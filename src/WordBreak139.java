29dc4a536419909fbdc8b2b36cd450334936c992
fluency03 <fluency.03@gmail.com>
2017-07-10 00:52:55 +0200

    WordBreak139

---
+++ src/WordBreak139.java
@@ -0,0 +1,46 @@
+/**
+ * Given a non-empty string s and a dictionary wordDict containing a list of
+ * non-empty words, determine if s can be segmented into a space-separated
+ * sequence of one or more dictionary words. You may assume the dictionary
+ * does not contain duplicate words.
+ *
+ * For example, given
+ * s = "leetcode",
+ * dict = ["leet", "code"].
+ *
+ * Return true because "leetcode" can be segmented as "leet code".
+ *
+ * UPDATE (2017/1/4):
+ * The wordDict parameter had been changed to a list of strings (instead of a
+ * set of strings). Please reload the code definition to get the latest changes.
+ */
+
+
+public class WordBreak139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int L = s.length();
+        if (L == 0) return true;
+        boolean[] dp = new boolean[L+1];
+
+        dp[0] = true;
+        int k = 0;
+        for (int i=0; i<=L; i++) {
+            for (int j=0; j<=i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+
+        }
+
+        return dp[L];
+    }
+
+
+
+
+
+
+
+
+}
