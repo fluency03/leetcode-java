1d8ef4ffe77c8158ea2745b493123dbd8ae8f331
fluency03 <fluency.03@gmail.com>
2017-07-13 23:02:32 +0200

    DeleteNodeInABST450

--- src/DeleteNodeInABST450.java
+++ src/DeleteNodeInABST450.java
@@ -95,5 +95,41 @@ public class DeleteNodeInABST450 {
 
 
 
+    public TreeNode deleteNode2(TreeNode root, int key) {
+        /* Base Case: If the tree is empty */
+        if (root == null)  return root;
+
+        /* Otherwise, recur down the tree */
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            // node with only one child or no child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // node with two children: Get the inorder successor (smallest
+            // in the right subtree)
+            root.val = minValue(root.right);
+
+            // Delete the inorder successor
+            root.right = deleteNode(root.right, root.val);
+        }
+
+        return root;
+    }
+
+    private int minValue(TreeNode root) {
+        int minv = root.val;
+        while (root.left != null) {
+            minv = root.left.val;
+            root = root.left;
+        }
+        return minv;
+    }
 
 }
