4ba60714329e6a91add656b4927c9c1ec9d01829
fluency03 <fluency.03@gmail.com>
2017-07-19 23:51:45 +0200

    InsertionSort

---
+++ src/sort/InsertionSort.java
@@ -0,0 +1,40 @@
+/**
+ * InsertionSort: for every element in the array, insert it into sorted sequence iteratively.
+ *
+ * Time Complexity: O(n^2).
+ * Space Complexity: O(1).
+ *
+ */
+
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void sort(int[] arr) {
+        for (int i=1; i < arr.length; i++) {
+            int curr = arr[i];
+            int j = i-1;
+
+            while (j >= 0 && arr[j] > curr) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = curr;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {10, 3, 7, 5, 1, 15, 20};
+        InsertionSort.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        int[] arr2 = {};
+        InsertionSort.sort(arr2);
+        System.out.println(Arrays.toString(arr2));
+
+        int[] arr3 = {10};
+        InsertionSort.sort(arr3);
+        System.out.println(Arrays.toString(arr3));
+    }
+
+}
