f0bc75c9c41f323dee6823cbc5eca351b032a440
fluency03 <fluency.03@gmail.com>
2017-07-20 15:31:08 +0200

    MergeSort

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -12,6 +12,8 @@ import java.util.Arrays;
 public class MergeSort {
     public static void sort(int[] arr) {
         mergeSort(arr, 0, arr.length-1);
+        // int[] helper = new int[arr.length];
+        // mergeSort(arr, helper, 0, arr.length-1);
     }
 
     private static void mergeSort(int[] arr, int l, int r) {
@@ -19,7 +21,7 @@ public class MergeSort {
 
         int m = (l+r)/2;
         mergeSort(arr, l, m);
-        mergeSort(arr, m+1, l);
+        mergeSort(arr, m+1, r);
 
         merge(arr, l, m, r);
     }
@@ -31,12 +33,13 @@ public class MergeSort {
         int L[] = new int [len1];
         int R[] = new int [len2];
 
-        for (int i=0; i < len1; ++i)
+        for (int i=0; i < len1; i++)
             L[i] = arr[l + i];
-        for (int j=0; j < len2; ++j)
-            R[j] = arr[m + 1+ j];
+        for (int j=0; j < len2; j++)
+            R[j] = arr[m + 1 + j];
 
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
         int k = l;
         while (i < len1 && j < len2) {
             if (L[i] <= R[j]) {
@@ -62,8 +65,45 @@ public class MergeSort {
         }
     }
 
+    //
+    private static void mergeSort(int[] arr, int[] helper, int l, int r) {
+        if (l >= r) return;
+
+        int m = (l+r)/2;
+        mergeSort(arr, helper, l, m);
+        mergeSort(arr, helper, m+1, r);
+
+        merge(arr, helper, l, m, r);
+    }
+
+    private static void merge(int[] arr, int[] helper, int l, int m, int r) {
+        for (int i = l; i <= r; i++) {
+            helper[i] = arr[i];
+        }
+
+        int i = l;
+        int j = m + 1;
+        int k = l;
+        while (i <= m && j <= r) {
+            if (helper[i] <= helper[j]) {
+                arr[k] = helper[i];
+                i++;
+            } else {
+                arr[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= m) {
+            arr[k] = helper[i];
+            k++;
+            i++;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr1 = {10, 3, 7, 5, 1, 15, 20};
+        int[] arr1 = {10, 3, 7, 5, 20, 15, 1};
         MergeSort.sort(arr1);
         System.out.println(Arrays.toString(arr1));
 
