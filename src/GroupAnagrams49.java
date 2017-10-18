6683134290903b4f9d5394920333f719808309f9
fluency03 <fluency.03@gmail.com>
2017-10-18 22:01:45 +0200

    GroupAnagrams49

--- src/GroupAnagrams49.java
+++ src/GroupAnagrams49.java
@@ -32,7 +32,30 @@ public class GroupAnagrams49 {
         return String.valueOf(chars);
     }
 
+    /**
+     * https://discuss.leetcode.com/topic/45639/java-beat-100-use-prime-number
+     */
+    public static List<List<String>> groupAnagrams2(String[] strs) {
+        int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103}; //最多10609个z
 
-
+        List<List<String>> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (String s : strs) {
+            int key = 1;
+            for (char c : s.toCharArray()) {
+                key *= prime[c - 'a'];
+            }
+            List<String> t;
+            if (map.containsKey(key)) {
+                t = res.get(map.get(key));
+            } else {
+                t = new ArrayList<>();
+                res.add(t);
+                map.put(key, res.size() - 1);
+            }
+            t.add(s);
+        }
+        return res;
+    }
 
 }
