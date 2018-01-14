e185307fcc509a93c3fa8a34f3c43cc15776043e
fluency03 <fluency.03@gmail.com>
2018-01-14 14:04:29 +0100

    122

---
+++ src/BestTimeToBuyAndSellStock122.java
@@ -0,0 +1,65 @@
+/**
+ * Say you have an array for which the ith element is the price of a given stock
+ * on day i.
+ *
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (ie, buy one and sell one share of the stock
+ * multiple times). However, you may not engage in multiple transactions at
+ * the same time (ie, you must sell the stock before you buy again).
+ *
+ */
+
+
+public class BestTimeToBuyAndSellStock121 {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+
+        int[] buy = new int[prices.length];
+        int[] sell = new int[prices.length];
+
+        buy[0] = -prices[0];
+
+        for (int i=1; i<prices.length; i++) {
+            buy[i] = Math.max(buy[i-1], sell[i-1]-prices[i]);
+            sell[i] = Math.max(sell[i-1], buy[i-1]+prices[i]);
+        }
+
+        return sell[prices.length-1];
+    }
+
+
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+
+        int buy = -prices[0];
+        int preBuy = -prices[0];
+        int sell = 0;
+        int preSell = 0;
+
+        for (int i=1; i<prices.length; i++) {
+            preBuy = buy;
+            preSell = sell;
+            buy = Math.max(preBuy, preSell - prices[i]);
+            sell = Math.max(preSell, preBuy + prices[i]);
+        }
+
+        return sell;
+    }
+
+
+    /**
+     * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solution/
+     */
+    public int maxProfit(int[] prices) {
+        int profit=0;
+
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                profit+= prices[i]-prices[i-1];
+            }
+        }
+        return profit;
+    }
+
+
+}
