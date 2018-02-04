a43249e27c37847c8e36987a457d12b4229e7341
fluency03 <fluency.03@gmail.com>
2018-02-04 22:12:53 +0100

    MostFrequentSubtreeSum508

--- README.md
+++ README.md
@@ -32,11 +32,11 @@
 
 
 
-# Total: 142
+# Total: 143
 
 | Easy | Medium | Hard |
 |:----:|:------:|:----:|
-|  23  |   89   |  30  |
+|  23  |   90   |  30  |
 
 
 | Question | Solution | Difficulty |
@@ -171,6 +171,7 @@
 | [450. Delete Node in a BST](https://leetcode.com/problems/delete-node-in-a-bst/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/DeleteNodeInABST450.java) | Medium |
 | [477. Total Hamming Distance](https://leetcode.com/problems/total-hamming-distance) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/TotalHammingDistance477.java) | Medium |
 | [480. Sliding Window Median](https://leetcode.com/problems/sliding-window-median/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/SlidingWindowMedian480.java) | Hard |
+| [508. Most Frequent Subtree Sum](https://leetcode.com/problems/most-frequent-subtree-sum/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/MostFrequentSubtreeSum508.java) | Medium |
 | [513. Find Bottom Left Tree Value](https://leetcode.com/problems/find-bottom-left-tree-value/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/FindBottomLeftTreeValue513.java) | Medium |
 | [525. Contiguous Array](https://leetcode.com/problems/contiguous-array/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/ContiguousArray525.java) | Medium |
 | [535. Encode and Decode TinyURL](https://leetcode.com/problems/encode-and-decode-tinyurl/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/EncodeAndDecodeTinyURL535.java) | Medium |
---
+++ src/MostFrequentSubtreeSum508.java
@@ -0,0 +1,76 @@
+/**
+ * Given the root of a tree, you are asked to find the most frequent subtree
+ * sum. The subtree sum of a node is defined as the sum of all the node values
+ * formed by the subtree rooted at that node (including the node itself). So
+ * what is the most frequent subtree sum value? If there is a tie, return all
+ * the values with the highest frequency in any order.
+ *
+ * Examples 1
+ * Input:
+ *
+ *   5
+ *  /  \
+ * 2   -3
+ * return [2, -3, 4], since all the values happen only once, return all of
+ * them in any order.
+ *
+ * Examples 2
+ * Input:
+ *
+ *   5
+ *  /  \
+ * 2   -5
+ * return [2], since 2 happens twice, however -5 only occur once.
+ *
+ * Note: You may assume the sum of values in any subtree is in the range of
+ * 32-bit signed integer.
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+public class MostFrequentSubtreeSum508 {
+    public int[] findFrequentTreeSum(TreeNode root) {
+        Map<Integer, Integer> sums = new HashMap<>();
+        if (root == null) return new int[0];
+
+        int[] cache = new int[1];
+        cache[0] = Integer.MIN_VALUE;
+        helper(root, sums, cache);
+
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> e: sums.entrySet()) {
+            // System.out.println(e.getKey() + " : " + e.getValue());
+            if (e.getValue().equals(cache[0])) {
+                list.add(e.getKey());
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i=0; i<list.size(); i++) res[i] = list.get(i);
+        return res;
+    }
+
+    private Integer helper(TreeNode n, Map<Integer, Integer> sums, int[] cache) {
+        if (n == null) return null;
+
+        Integer left = helper(n.left, sums, cache);
+        Integer right = helper(n.right, sums, cache);
+
+        int sum = ((left == null) ? 0 : left) + ((right == null) ? 0 : right) + n.val;
+        int t = sums.getOrDefault(sum, 0) + 1;
+        cache[0] = Math.max(cache[0], t);
+        sums.put(sum, t);
+
+        return sum;
+    }
+
+}
