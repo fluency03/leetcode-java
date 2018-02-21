5ae1297eefac3c5167c71e7c7fbb1ad274e64b83
fluency03 <fluency.03@gmail.com>
2018-02-21 17:39:27 +0800

    MergeTwoSortedLists21

--- src/sort/MergeTwoSortedLists21.java
+++ src/sort/MergeTwoSortedLists21.java
@@ -47,6 +47,19 @@ public class MergeTwoSortedLists21 {
     }
 
 
-
+    /**
+     * https://leetcode.com/problems/merge-two-sorted-lists/discuss/9715/Java-1-ms-4-lines-codes-using-recursion
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2){
+    		if(l1 == null) return l2;
+    		if(l2 == null) return l1;
+    		if(l1.val < l2.val){
+    			l1.next = mergeTwoLists2(l1.next, l2);
+    			return l1;
+    		} else{
+    			l2.next = mergeTwoLists2(l1, l2.next);
+    			return l2;
+    		}
+    }
 
 }
