f32fcb58af33da42e5cc31311430dc831535a12e
fluency03 <fluency.03@gmail.com>
2018-02-22 15:39:00 +0800

    48. Rotate Image

--- src/RotateImage48.java
+++ src/RotateImage48.java
@@ -67,4 +67,26 @@ public class RotateImage48 {
     }
 
 
+    /**
+     * https://leetcode.com/problems/rotate-image/discuss/18879/AC-Java-in-place-solution-with-explanation-Easy-to-understand.
+     */
+    public void rotate2(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0 ; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+
 }
