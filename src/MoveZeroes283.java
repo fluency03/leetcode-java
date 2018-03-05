54cffbcbafc9a9e8b48a1016c35280e043c27754
fluency03 <fluency.03@gmail.com>
2018-03-05 20:53:50 +0100

    MoveZeroes283

--- src/MoveZeroes283.java
+++ src/MoveZeroes283.java
@@ -28,4 +28,38 @@ public class MoveZeroes283 {
             slow++;
         }
     }
+
+    public void moveZeroes2(int[] nums) {
+        int p = 0;
+        int zeros = -1;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == 0) {
+                zeros++;
+            } else {
+                nums[p] = nums[i];
+                p++;
+            }
+
+        }
+
+        while (zeros >=0) {
+            nums[nums.length-1-zeros] = 0;
+            zeros--;
+        }
+    }
+
+    public void moveZeroes3(int[] nums) {
+        for (int i = 0, p = 0; p < nums.length; p++) {
+            if (nums[p] != 0) {
+                swap(nums, i++, p);
+            }
+        }
+    }
+
+    private void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
 }
