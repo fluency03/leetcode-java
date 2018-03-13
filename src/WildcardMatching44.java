da0edf320d76b17c509250845272703944e62b96
fluency03 <fluency.03@gmail.com>
2018-03-14 00:33:54 +0100

    isMatch4

--- src/WildcardMatching44.java
+++ src/WildcardMatching44.java
@@ -133,20 +133,19 @@ public class WildcardMatching44 {
                 match = s;
                 p++;
             }
-           // last pattern pointer was *, advancing string pointer
+            // last pattern pointer was *, advancing string pointer
             else if (starIdx != -1){
                 p = starIdx + 1;
                 match++;
                 s = match;
             }
-           //current pattern pointer is not star, last patter pointer was not *
-          //characters do not match
+            //current pattern pointer is not star, last patter pointer was not *
+            //characters do not match
             else return false;
         }
 
         //check for remaining characters in pattern
-        while (p < pattern.length() && pattern.charAt(p) == '*')
-            p++;
+        while (p < pattern.length() && pattern.charAt(p) == '*') p++;
 
         return p == pattern.length();
     }
