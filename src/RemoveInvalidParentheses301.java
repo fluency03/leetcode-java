1dacde18a9e4d9c5f6e74d574e55757ab96bfc20
fluency03 <fluency.03@gmail.com>
2018-03-17 19:15:54 +0100

    RemoveInvalidParentheses301

--- src/RemoveInvalidParentheses301.java
+++ src/RemoveInvalidParentheses301.java
@@ -11,6 +11,18 @@
  */
 
 
+/**
+ * A review of all solutions (BFS and DFS)
+ * https://leetcode.com/problems/remove-invalid-parentheses/discuss/75038/Evolve-from-intuitive-solution-to-optimal-a-review-of-all-solutions
+ *
+ * There are three challenges:
+ *   - Remove minimum parenthesis
+ *   - The result is valid
+ *   - Without duplicates.
+ *
+ */
+
+
 public class RemoveInvalidParentheses301 {
     public List<String> removeInvalidParentheses(String s) {
         Set<String> results = new HashSet<>();
@@ -218,4 +230,75 @@ public class RemoveInvalidParentheses301 {
 
         sb.setLength(len);
     }
+
+
+    /**
+     * https://leetcode.com/problems/remove-invalid-parentheses/discuss/75095/Java-optimized-DFS-solution-3-ms
+     */
+    public List<String> removeInvalidParentheses5(String s) {
+         int count = 0, openN = 0, closeN = 0;
+
+         // calculate the total numbers of opening and closing parentheses
+         // that need to be removed in the final solution
+         for (char c : s.toCharArray()) {
+             if (c == '(') {
+                 count++;
+             } else if (c == ')') {
+                 if (count == 0) closeN++;
+                 else count--;
+             }
+         }
+         openN = count;
+         count = 0;
+
+         if (openN == 0 && closeN == 0) return Arrays.asList(s);
+
+         List<String> result = new ArrayList<>();
+         StringBuilder sb = new StringBuilder();
+
+         dfs(s.toCharArray(), 0, count, openN, closeN, result, sb);
+
+         return result;
+     }
+
+     private void dfs(char[] s, int p, int count, int openN, int closeN, List<String> result, StringBuilder sb) {
+         if (count < 0) return; // the parentheses is invalid
+
+         if (p == s.length) {
+             if (openN == 0 && closeN == 0) { // the minimum number of invalid parentheses have been removed
+                 result.add(sb.toString());
+             }
+             return;
+         }
+
+         if (s[p] != '(' && s[p] != ')') {
+             sb.append(s[p]);
+             dfs(s, p + 1, count, openN, closeN, result, sb);
+             sb.deleteCharAt(sb.length() - 1);
+         } else if (s[p] == '(') {
+             int i = 1;
+             while (p + i < s.length && s[p + i] == '(') i++; // use while loop to avoid duplicate result in DFS, instead of using HashSet
+             sb.append(s, p, i);
+             dfs(s, p + i, count + i, openN, closeN, result, sb);
+             sb.delete(sb.length() - i, sb.length());
+
+             if (openN > 0) {
+                 // remove the current opening parenthesis
+                 dfs(s, p + 1, count, openN - 1, closeN, result, sb);
+             }
+         } else {
+             int i = 1;
+             while (p + i < s.length && s[p + i] == ')') i++; // use while loop to avoid duplicate result in DFS, instead of using HashSet
+             sb.append(s, p, i);
+             dfs(s, p + i, count - i, openN, closeN, result, sb);
+             sb.delete(sb.length() - i, sb.length());
+
+             if (closeN > 0) {
+                 // remove the current closing parenthesis
+                 dfs(s, p + 1, count, openN, closeN - 1, result, sb);
+             }
+         }
+     }
+
+
 }
