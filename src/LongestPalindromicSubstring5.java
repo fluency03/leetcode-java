715b4708c9c29b3e5f37573263b64e03fc2ef846
fluency03 <fluency.03@gmail.com>
2018-04-03 16:48:22 +0200

    LongestPalindromicSubstring5

--- src/LongestPalindromicSubstring5.java
+++ src/LongestPalindromicSubstring5.java
@@ -77,4 +77,27 @@ public class LongestPalindromicSubstring5 {
       	}
     }
 
+
+    /**
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/2921/Share-my-Java-solution-using-dynamic-programming
+     */
+    public String longestPalindrome3(String s) {
+      int n = s.length();
+      String res = null;
+
+      boolean[][] dp = new boolean[n][n];
+
+      for (int i = n - 1; i >= 0; i--) {
+        for (int j = i; j < n; j++) {
+          dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+          if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+            res = s.substring(i, j + 1);
+          }
+        }
+      }
+
+      return res;
+    }
+
 }
