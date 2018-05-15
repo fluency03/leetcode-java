0097b6aa810ebb5b655aa04dfee6836f590f9e4c
fluency03 <fluency.03@gmail.com>
2018-05-16 00:05:05 +0200

    HIndex274

--- src/HIndex274.java
+++ src/HIndex274.java
@@ -136,4 +136,20 @@ public class HIndex274 {
         return 0;
     }
 
+    /**
+     * https://leetcode.com/problems/h-index/solution/
+     */
+    public int hIndex6(int[] citations) {
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // counting papers for each citation number
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // finding the h-index
+        int k = n;
+        for (int s = papers[n]; k > s; s += papers[k])
+            k--;
+        return k;
+    }
+
 }
