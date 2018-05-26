42c5fda93d123d97fe713350b326d86f71f4f8a6
fluency03 <fluency.03@gmail.com>
2018-05-26 14:11:59 +0200

    SplitArrayIntoConsecutiveSubsequences659

--- src/SplitArrayIntoConsecutiveSubsequences659.java
+++ src/SplitArrayIntoConsecutiveSubsequences659.java
@@ -102,6 +102,29 @@ public class SplitArrayIntoConsecutiveSubsequences659 {
     }
 
 
-
+    /**
+     * https://leetcode.com/problems/split-array-into-consecutive-subsequences/discuss/106495/Java-O(n)-time-and-O(1)-space-solution-greedily-extending-shorter-subsequence
+     */
+    public boolean isPossible2(int[] nums) {
+        int pre = Integer.MIN_VALUE, p1 = 0, p2 = 0, p3 = 0;
+        int cur = 0, cnt = 0, c1 = 0, c2 = 0, c3 = 0;
+            
+        for (int i = 0; i < nums.length; pre = cur, p1 = c1, p2 = c2, p3 = c3) {
+            for (cur = nums[i], cnt = 0; i < nums.length && cur == nums[i]; cnt++, i++);
+                
+            if (cur != pre + 1) {
+                if (p1 != 0 || p2 != 0) return false;
+                c1 = cnt; c2 = 0; c3 = 0;
+                
+            } else {
+                if (cnt < p1 + p2) return false;
+                c1 = Math.max(0, cnt - (p1 + p2 + p3));
+                c2 = p1;
+                c3 = p2 + Math.min(p3, cnt - (p1 + p2));
+            }
+        }
+        
+        return (p1 == 0 && p2 == 0);
+    }
 
 }
