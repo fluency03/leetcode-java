de26b06890cd25ed12001a31b2b30e3eff2751f0
fluency03 <fluency.03@gmail.com>
2018-05-30 00:34:27 +0200

    WordSquares425

--- README.md
+++ README.md
@@ -40,11 +40,11 @@
 
 
 
-# Total: 237
+# Total: 238
 
 | Easy |  Medium | Hard | - |
 |:----:|:-------:|:----:|:-:|
-|  58  |   137   |  40  | 2 |
+|  58  |   137   |  41  | 2 |
 
 
 | Question | Solution | Difficulty |
@@ -246,6 +246,7 @@
 | [414. Third Maximum Number](https://leetcode.com/problems/third-maximum-number/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/ThirdMaximumNumber414.java) | Easy |
 | [416. Partition Equal Subset Sum](https://leetcode.com/problems/partition-equal-subset-sum/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/PartitionEqualSubsetSum416.java) | Medium |
 | [423. Reconstruct Original Digits from English](https://leetcode.com/problems/reconstruct-original-digits-from-english/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/ReconstructOriginalDigitsFromEnglish423.java) | Medium |
+| [425. Word Squares](https://leetcode.com/problems/word-squares/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/WordSquares425.java) | Hard |
 | [433. Minimum Genetic Mutation](https://leetcode.com/problems/minimum-genetic-mutation/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/MinimumGeneticMutation433.java) | Medium |
 | [450. Delete Node in a BST](https://leetcode.com/problems/delete-node-in-a-bst/) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/DeleteNodeInABST450.java) | Medium |
 | [477. Total Hamming Distance](https://leetcode.com/problems/total-hamming-distance) | [Solution](https://github.com/fluency03/leetcode-java/blob/master/src/TotalHammingDistance477.java) | Medium |
---
+++ src/WordSquares425.java
@@ -0,0 +1,158 @@
+/**
+ * Given a set of words (without duplicates), find all word squares you can
+ * build from them.
+ * 
+ * A sequence of words forms a valid word square if the kth row and column read
+ * the exact same string, where 0 ≤ k < max(numRows, numColumns).
+ * 
+ * For example, the word sequence ["ball","area","lead","lady"] forms a word
+ * square because each word reads the same both horizontally and vertically.
+ * 
+ * b a l l
+ * a r e a
+ * l e a d
+ * l a d y
+ * 
+ * Note:
+ * - There are at least 1 and at most 1000 words.
+ * - All words will have the exact same length.
+ * - Word length is at least 1 and at most 5.
+ * - Each word contains only lowercase English alphabet a-z.
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * ["area","lead","wall","lady","ball"]
+ * 
+ * Output:
+ * [
+ *   [ "wall",
+ *     "area",
+ *     "lead",
+ *     "lady"
+ *   ],
+ *   [ "ball",
+ *     "area",
+ *     "lead",
+ *     "lady"
+ *   ]
+ * ]
+ * 
+ * Explanation:
+ * The output consists of two word squares. The order of output does not matter
+ * (just the order of words in each word square matters).
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * ["abat","baba","atan","atal"]
+ * 
+ * Output:
+ * [
+ *   [ "baba",
+ *     "abat",
+ *     "baba",
+ *     "atan"
+ *   ],
+ *   [ "baba",
+ *     "abat",
+ *     "baba",
+ *     "atal"
+ *   ]
+ * ]
+ * 
+ * Explanation:
+ * The output consists of two word squares. The order of output does not matter
+ * (just the order of words in each word square matters).
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WordSquares425 {
+    public List<List<String>> wordSquares(String[] words) {
+        List<List<String>> res = new ArrayList<>();
+        if (words == null || words.length == 0 || words[0].length() == 0) return res;
+
+        int len = words[0].length();
+        TrieNode root = new TrieNode();
+        for (String word: words) root.addWord(word);
+
+        helper(new char[len][len], root, 0, len, new ArrayList<String>(), res);
+        return res;
+    }
+
+    private void helper(char[][] square, TrieNode root, int pos, int len, List<String> sqr, List<List<String>> res) {
+        if (pos == len) {
+            if (sqr.size() == len) {
+                List<String> newSqr = new ArrayList<String>();
+                for (String s: sqr) {
+                    newSqr.add(s);
+                }
+                res.add(newSqr);
+            }
+            return;
+        }
+        List<String> finds = root.search(Arrays.copyOfRange(square[pos], 0, pos), len);
+
+        if (finds.size() == 0) return;
+        for (String w: finds) {
+            sqr.add(w);
+            for (int j=pos; j<len; j++) square[j][pos] = w.charAt(j);
+            helper(square, root, pos+1, len, sqr, res);
+            sqr.remove(sqr.size() - 1);
+        }
+    }
+
+
+    class TrieNode {
+        boolean isWord = false;
+        String word = null;
+        TrieNode[] children = new TrieNode[26];
+    
+        public void addWord(String word) {
+            addWord(word.toCharArray(), 0);
+        }
+    
+        public void addWord(char[] chars, int i) {
+            if (i == chars.length) {
+                this.isWord = true;
+                this.word = new String(chars);
+                return;
+            }
+            int idx = chars[i] - 'a';
+            if (children[idx] == null) {
+                TrieNode t = new TrieNode();
+                children[idx] = t;
+            }
+            children[idx].addWord(chars, i+1);
+        }
+    
+        public List<String> search(char[] prefix, int length) {
+            List<String> res = new ArrayList<String>();
+            search(prefix, length, 0, res);
+            return res;
+        }
+
+        public void search(char[] prefix, int length, int i, List<String> res) {
+            if (i == length) {
+                if (word != null) res.add(word);
+                return;
+            }
+            if (i < prefix.length) {
+                if (children[prefix[i] - 'a'] == null) return;
+                children[prefix[i] - 'a'].search(prefix, length, i+1, res);
+            } else {
+                for (TrieNode child: children) {
+                    if (child != null) child.search(prefix, length, i+1, res);
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
