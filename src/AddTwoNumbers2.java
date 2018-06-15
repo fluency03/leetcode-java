e68585184c6990c6d222fbcc167fbc02dfd76fe7
fluency03 <fluency.03@gmail.com>
2018-06-15 21:50:53 +0200

    AddTwoNumbers2

--- src/AddTwoNumbers2.java
+++ src/AddTwoNumbers2.java
@@ -33,4 +33,36 @@ public class AddTwoNumbers2 {
         return dummy.next;
     }
 
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        int carry = 0;
+        while (l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + carry;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            carry = sum / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        
+        while (l1 != null) {
+            int sum = l1.val + carry;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            carry = sum / 10;
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            int sum = l2.val + carry;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            carry = sum / 10;
+            l2 = l2.next;
+        }
+        if (carry != 0) p.next =  new ListNode(carry);
+        
+        return dummy.next;
+    }
+
 }
