b97613ae5f944f98c07a0bd86351064c005f7baf
fluency03 <fluency.03@gmail.com>
2018-06-20 21:00:58 +0200

    public class SubtreeOfAnotherTree572 {

--- src/SubtreeOfAnotherTree572.java
+++ src/SubtreeOfAnotherTree572.java
@@ -50,38 +50,66 @@
 
 public class SubtreeOfAnotherTree572 {
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        if (t == null) {
-            return true;
-        }
-
-        if (s == null) {
-            return false;
-        }
-
-        if (s.val == t.val && isSame(s, t)) {
-            return true;
-        }
-
-        boolean left = isSubtree(s.left, t);
-        if (left == true) {
-            return true;
-        }
-        return isSubtree(s.right, t);
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
 
     public boolean isSame(TreeNode s, TreeNode t) {
-        if (s == null && t == null) {
-            return true;
-        }
-
-        if (s == null || t == null) {
-            return false;
-        }
-
-        if (s.val != t.val) {
-            return false;
-        }
-
+        if (s == null) return t == null;
+        if (t == null) return s == null;
+        if (s.val != t.val) return false;
         return isSame(s.left, t.left) && isSame(s.right, t.right);
     }
+
+
+    /**
+     * https://leetcode.com/problems/subtree-of-another-tree/discuss/102760/Easy-O(n)-java-solution-using-preorder-traversal
+     */
+    public boolean isSubtree2(TreeNode s, TreeNode t) {
+        String spreorder = generatepreorderString(s); 
+        String tpreorder = generatepreorderString(t);
+        return spreorder.contains(tpreorder) ;
+    }
+
+    public String generatepreorderString(TreeNode s){
+        StringBuilder sb = new StringBuilder();
+        Stack<TreeNode> stacktree = new Stack();
+        stacktree.push(s);
+        while(!stacktree.isEmpty()){
+           TreeNode popelem = stacktree.pop();
+           if(popelem==null)
+              sb.append(",#"); // Appending # inorder to handle same values but not subtree cases
+           else      
+              sb.append(","+popelem.val);
+           if(popelem!=null){
+                stacktree.push(popelem.right);    
+                stacktree.push(popelem.left);  
+           }
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * https://leetcode.com/problems/subtree-of-another-tree/discuss/102736/Java-Concise-O(n+m)-Time-O(n+m)-Space
+     */
+    public boolean isSubtree3(TreeNode s, TreeNode t) {
+        return serialize(s).contains(serialize(t)); // Java uses a naive contains algorithm so to ensure linear time, 
+                                                    // replace with KMP algorithm
+    }
+    
+    public String serialize(TreeNode root) {
+        StringBuilder res = new StringBuilder();
+        serialize(root, res);
+        return res.toString();
+    }
+    
+    private void serialize(TreeNode cur, StringBuilder res) {
+        if (cur == null) {res.append(",#"); return;}
+        res.append("," + cur.val);
+        serialize(cur.left, res);
+        serialize(cur.right, res);
+    }
+
 }
