39da13ba34ad2abceda05282b79f00c6122ccf9e
fluency03 <fluency.03@gmail.com>
2018-06-23 18:53:53 +0200

    WordLadder127

--- src/WordLadder127.java
+++ src/WordLadder127.java
@@ -36,6 +36,45 @@ import java.util.HashSet;
 public class WordLadder127 {
     // DFS too slow
 
+    // BFS
+    public int ladderLength(String start, String end, List<String> dictL) {
+        Set<String> dict = new HashSet<>(dictL);
+        if (!dict.contains(end)) {
+            return 0;
+        }
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(start);
+
+        // Mark visited word
+        Set<String> visited = new HashSet<String>();
+        visited.add(start);
+
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int q=0; q<size; q++) {
+                String str = queue.poll(); 
+                // Modify str's each character (so word distance is 1)
+                for (int i = 0; i < str.length(); i++) {
+                    char[] chars = str.toCharArray();
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chars[i] = c;
+                        String word = new String(chars);
+                        // Found the end word
+                        if (word.equals(end)) return level + 1;
+                        // Put it to the queue
+                        if (dict.contains(word) && !visited.contains(word)) {
+                            queue.add(word);
+                            visited.add(word);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+
     /**
      * https://discuss.leetcode.com/topic/17890/another-accepted-java-solution-bfs
      */
