7d31807d4186f853c8e98b40f89ca7c72b812bba
fluency03 <fluency.03@gmail.com>
2018-06-23 15:51:02 +0200

    PalindromePartitioning131

--- src/PalindromePartitioning131.java
+++ src/PalindromePartitioning131.java
@@ -46,5 +46,39 @@ public class PalindromePartitioning131 {
         return true;
     }
 
+
+    /**
+     * https://leetcode.com/problems/palindrome-partitioning/discuss/41982/Java-DP-+-DFS-solution
+     */
+    public List<List<String>> partition2(String s) {
+        List<List<String>> res = new ArrayList<>();
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 0; j <= i; j++) {
+                if(s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j+1][i-1])) {
+                    dp[j][i] = true;
+                }
+            }
+        }
+        helper(res, new ArrayList<>(), dp, s, 0);
+        return res;
+    }
+    
+    private void helper(List<List<String>> res, List<String> path, boolean[][] dp, String s, int pos) {
+        if(pos == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int i = pos; i < s.length(); i++) {
+            if(dp[pos][i]) {
+                path.add(s.substring(pos,i+1));
+                helper(res, path, dp, s, i+1);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+
+
 }
 
