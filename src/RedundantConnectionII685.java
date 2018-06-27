f8739dc421dfe9de5a10e2255e2647c58e37029b
fluency03 <fluency.03@gmail.com>
2018-06-27 22:37:16 +0200

    RedundantConnectionII685

--- src/RedundantConnectionII685.java
+++ src/RedundantConnectionII685.java
@@ -82,7 +82,6 @@ public class RedundantConnectionII685 {
         return res2;
     }
 
-    
     class DisjointSet {
         int[] parent;
         int[] rank;
@@ -112,6 +111,50 @@ public class RedundantConnectionII685 {
                 rank[yp]++;
             }
         }
+    }
+
+
+    /**
+     * https://leetcode.com/problems/redundant-connection-ii/discuss/108045/C++Java-Union-Find-with-explanation-O(n)
+     */
+    public int[] findRedundantDirectedConnection2(int[][] edges) {
+        int[] can1 = {-1, -1};
+        int[] can2 = {-1, -1};
+        int[] parent = new int[edges.length + 1];
+        for (int i = 0; i < edges.length; i++) {
+            if (parent[edges[i][1]] == 0) {
+                parent[edges[i][1]] = edges[i][0];
+            } else {
+                can2 = new int[] {edges[i][0], edges[i][1]};
+                can1 = new int[] {parent[edges[i][1]], edges[i][1]};
+                edges[i][1] = 0;
+            }
+        }
+        for (int i = 0; i < edges.length; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < edges.length; i++) {
+            if (edges[i][1] == 0) {
+                continue;
+            }
+            int child = edges[i][1], father = edges[i][0];
+            if (root(parent, father) == child) {
+                if (can1[0] == -1) {
+                    return edges[i];
+                }
+                return can1;
+            }
+            parent[child] = father;
+        }
+        return can2;
+    }
+    
+    int root(int[] parent, int i) {
+        while (i != parent[i]) {
+            parent[i] = parent[parent[i]];
+            i = parent[i];
+        }   
+        return i;
     }
 
 }
