9de5357618efd85d3c2a3b19be55c1070ed1d0d2
fluency03 <fluency.03@gmail.com>
2018-07-12 22:43:46 +0200

    MaximumSubarray53

--- src/MaximumSubarray53.java
+++ src/MaximumSubarray53.java
@@ -38,7 +38,7 @@ public class MaximumSubarray53 {
     /**
      * https://leetcode.com/problems/maximum-subarray/discuss/20210/O(n)-Java-solution
      */
-    public int maxSubArray(int[] A) {
+    public int maxSubArray2(int[] A) {
         int max = Integer.MIN_VALUE, sum = 0;
         for (int i = 0; i < A.length; i++) {
             if (sum < 0)
@@ -49,6 +49,34 @@ public class MaximumSubarray53 {
                 max = sum;
         }
         return max;
+    }
+
+
+    // divide-and-conquer
+    public int maxSubArray3(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        return maxSubArray(nums, 0, nums.length-1);
+    }
+    
+    private int maxSubArray(int[] nums, int left, int right) {
+        if (left > right) return Integer.MIN_VALUE;
+        if (left == right) return nums[left];
+        int mid = (left + right) / 2;
+        int L = maxSubArray(nums, left, mid-1);
+        int R = maxSubArray(nums, mid+1, right);
+        int leftSum = 0;
+        int tmp = 0;
+        for (int i=mid-1; i>=left; i--) {
+            tmp += nums[i];
+            if (tmp > leftSum) leftSum = tmp;
+        }
+        tmp = 0;
+        int rightSum = 0;
+        for (int i=mid+1; i<=right; i++) {
+            tmp += nums[i];
+            if (tmp > rightSum) rightSum = tmp;
+        }
+        return Math.max(Math.max(L, R), leftSum + rightSum + nums[mid]);
     }
 
 
