812eb7906ba698998aff54084a54812cd0fb5711
fluency03 <fluency.03@gmail.com>
2018-07-13 00:25:55 +0200

    MaximumProductSubarray152

--- src/MaximumProductSubarray152.java
+++ src/MaximumProductSubarray152.java
@@ -79,4 +79,53 @@ public class MaximumProductSubarray152 {
     }
 
 
+    /**
+     * https://leetcode.com/problems/maximum-product-subarray/discuss/48330/Simple-Java-code
+     */
+    public int maxProduct3(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int max = A[0], min = A[0], result = A[0];
+        for (int i = 1; i < A.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * https://leetcode.com/problems/maximum-product-subarray/discuss/48230/Possibly-simplest-solution-with-O(n)-time-complexity
+     */
+    public int maxProduct4(int A[]) {
+        // store the result that is the max we have found so far
+        int r = A[0];
+    
+        // imax/imin stores the max/min product of
+        // subarray that ends with the current number A[i]
+        for (int i = 1, imax = r, imin = r; i < A.length; i++) {
+            // multiplied by a negative makes big number smaller, small number bigger
+            // so we redefine the extremums by swapping them
+            if (A[i] < 0) {
+                int t = imax;
+                imax = imin;
+                imin = t;
+            }
+    
+            // max/min product for the current number is either the current number itself
+            // or the max/min by the previous number times the current one
+            imax = Math.max(A[i], imax * A[i]);
+            imin = Math.min(A[i], imin * A[i]);
+    
+            // the newly computed max value is a candidate for our global result
+            r = Math.max(r, imax);
+        }
+        return r;
+    }
+
 }
