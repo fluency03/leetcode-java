5ef8267fb524ec137936c0df9ff5f23c776174b4
fluency03 <fluency.03@gmail.com>
2018-07-12 20:20:33 +0200

    ContainerWithMostWater11

--- src/ContainerWithMostWater11.java
+++ src/ContainerWithMostWater11.java
@@ -8,6 +8,33 @@
  */
 
 public class ContainerWithMostWater11 {
+    /**
+     * https://leetcode.com/problems/container-with-most-water/discuss/6089/Anyone-who-has-a-O(N)-algorithm/7268
+     * Here is the proof.
+     * Proved by contradiction:
+     * 
+     * Suppose the returned result is not the optimal solution. Then there must
+     * exist an optimal solution, say a container with a_ol and a_or (left and
+     * right respectively), such that it has a greater volume than the one we got.
+     * Since our algorithm stops only if the two pointers meet. So, we must have
+     * visited one of them but not the other. WLOG, let's say we visited a_ol but
+     * not a_or. When a pointer stops at a_ol, it won't move until
+     * 
+     * The other pointer also points to a_ol.
+     * In this case, iteration ends. But the other pointer must have visited a_or
+     * on its way from right end to a_ol. Contradiction to our assumption that we
+     * didn't visit a_or.
+     * 
+     * The other pointer arrives at a value, say a_rr, that is greater than a_ol
+     * before it reaches a_or.
+     * 
+     * In this case, we does move a_ol. But notice that the volume of a_ol and
+     * a_rr is already greater than a_ol and a_or (as it is wider and heigher),
+     * which means that a_ol and a_or is not the optimal solution --
+     * Contradiction!
+     * 
+     * Both cases arrive at a contradiction.
+     */
     public int maxArea(int[] height) {
         int res = 0;
         int n = height.length;
