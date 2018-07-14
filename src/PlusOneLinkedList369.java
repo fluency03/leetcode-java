d99d250618de9057bc052cc0f7334819e40ab1a0
fluency03 <fluency.03@gmail.com>
2018-07-15 00:57:44 +0200

    PlusOneLinkedList369

--- src/PlusOneLinkedList369.java
+++ src/PlusOneLinkedList369.java
@@ -44,4 +44,32 @@ public class PlusOneLinkedList369 {
     }
 
 
+    /**
+     * https://leetcode.com/problems/plus-one-linked-list/discuss/84150/Two-Pointers-Java-Solution:-O(n)-time-O(1)-space
+     */
+    public ListNode plusOne2(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode i = dummy;
+        ListNode j = dummy;
+
+        while (j.next != null) {
+            j = j.next;
+            if (j.val != 9) {
+                i = j;
+            }
+        }
+        // i = index of last non-9 digit
+    
+        i.val++;
+        i = i.next;
+        while (i != null) {
+            i.val = 0;
+            i = i.next;
+        }
+        
+        if (dummy.val == 0) return dummy.next;
+        return dummy;
+    }
+
 }
