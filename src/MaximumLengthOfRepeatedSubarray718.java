19be57611ee839de29e94930d306608ce4192811
fluency03 <fluency.03@gmail.com>
2018-07-14 21:50:14 +0200

    MaximumLengthOfRepeatedSubarray718

--- src/MaximumLengthOfRepeatedSubarray718.java
+++ src/MaximumLengthOfRepeatedSubarray718.java
@@ -82,4 +82,46 @@ public class MaximumLengthOfRepeatedSubarray718 {
         return max;
     }
 
+
+    public int findLength5(int[] A, int[] B) {
+        int lenA = A.length;
+        int lenB = B.length;
+        int res = 0;
+        int[][] dp = new int[lenA + 1][lenB + 1];
+        
+        for (int i=1; i<=lenA; i++) {
+            for (int j=1; j<=lenB; j++) {
+                if (A[i-1] == B[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    if (dp[i][j] > res) res = dp[i][j];
+                }
+            }
+        }
+        
+        return res;
+    }
+
+
+    public int findLength6(int[] A, int[] B) {
+        int lenA = A.length;
+        int lenB = B.length;
+        int res = 0;
+        int[] dp = new int[lenB + 1];
+        for (int i=1; i<=lenA; i++) {
+            int pre = 0;
+            for (int j=1; j<=lenB; j++) {
+                int tmp = dp[j];
+                if (A[i-1] == B[j-1]) {
+                    dp[j] = pre + 1;
+                    if (dp[j] > res) res = dp[j];
+                } else {
+                    dp[j] = 0;
+                }
+                pre = tmp;
+            }
+        }
+        
+        return res;
+    }
+
 }
