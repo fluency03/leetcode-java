4497bb14e7345e1f203e8c3a47e9153893724a1a
fluency03 <fluency.03@gmail.com>
2018-07-18 19:44:47 +0200

    BombEnemy361

--- src/BombEnemy361.java
+++ src/BombEnemy361.java
@@ -15,7 +15,7 @@
  * return 3. (Placing a bomb at (1,1) kills 3 enemies)
  */
 
-class BombEnemy361 {
+public class BombEnemy361 {
     public int maxKilledEnemies(char[][] grid) {
         if (grid == null || grid.length <= 0 || grid[0].length <= 0) return 0;
         int n = grid.length;
@@ -126,5 +126,84 @@ class BombEnemy361 {
 
         return h + v;
     }
-  
+
+
+    public int maxKilledEnemies3(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] left = new int[m][n];
+        int[][] right = new int[m][n];
+        int[][] top = new int[m][n];
+        int[][] bottom = new int[m][n];
+        
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j] != 'W') {
+                    int add = grid[i][j] == 'E' ? 1 : 0;
+                    left[i][j] = (j > 0 ? left[i][j-1] : 0) + add;
+                    top[i][j] = (i > 0 ? top[i-1][j] : 0) + add;
+                }
+                if (grid[i][n-j-1] != 'W') {
+                    right[i][n-j-1] = (j > 0 ? right[i][n-j] : 0) + (grid[i][n-j-1] == 'E' ? 1 : 0);
+                }
+                if (grid[m-i-1][j] != 'W') {
+                    bottom[m-i-1][j] = (i > 0 ? bottom[m-i][j] : 0) + (grid[m-i-1][j] == 'E' ? 1 : 0);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j] == '0') {
+                    int local = left[i][j] + right[i][j] + top[i][j] + bottom[i][j];
+                    if (local > res) {
+                        res = local;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public int maxKilledEnemies4(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int rowKills = 0;
+        int[] colKills = new int[n];
+        
+        int res = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (j == 0 || grid[i][j-1] == 'W') {
+                    int tempRow = 0;
+                    for (int k=j; k<n && grid[i][k] != 'W'; k++) {
+                        if (grid[i][k] == 'E') {
+                            tempRow++;
+                        }
+                    }
+                    rowKills = tempRow;
+                }
+                
+                if (i == 0 || grid[i-1][j] == 'W') {
+                    int tempCol = 0;
+                    for (int k=i; k<m && grid[k][j] != 'W'; k++) {
+                        if (grid[k][j] == 'E') {
+                            tempCol++;
+                        }
+                    }
+                    colKills[j] = tempCol;
+                }
+                
+                if (grid[i][j] == '0' && rowKills + colKills[j] > res) {
+                    res = rowKills + colKills[j];
+                }
+            }
+        }
+        return res;
+    }
+
 }
