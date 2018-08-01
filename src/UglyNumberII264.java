555b5051f38e33ba89c76927be52dfbfdf0dc626
fluency03 <fluency.03@gmail.com>
2018-08-01 20:41:01 +0200

    UglyNumberII264

--- src/UglyNumberII264.java
+++ src/UglyNumberII264.java
@@ -74,5 +74,25 @@ public class UglyNumberII264 {
         return ugly[n-1];
     }
 
+
+    /**
+     * https://leetcode.com/problems/ugly-number-ii/discuss/69372/Java-solution-using-PriorityQueue
+     */
+    public int nthUglyNumber3(int n) {
+        if(n==1) return 1;
+        PriorityQueue<Long> q = new PriorityQueue();
+        q.add(1l);
+        
+        for(long i=1; i<n; i++) {
+            long tmp = q.poll();
+            while(!q.isEmpty() && q.peek()==tmp) tmp = q.poll();
+            
+            q.add(tmp*2);
+            q.add(tmp*3);
+            q.add(tmp*5);
+        }
+        return q.poll().intValue();
+    }
+
 }
 
