68c5777d06e44a7cf2f12983029cbeddb22fbc87
fluency03 <fluency.03@gmail.com>
2018-08-04 11:30:48 +0200

    MaximumWidthOfBinaryTree662

--- src/MaximumWidthOfBinaryTree662.java
+++ src/MaximumWidthOfBinaryTree662.java
@@ -147,11 +147,23 @@ public class MaximumWidthOfBinaryTree662 {
     }
 
 
-
-
-
-
-
-
+    /**
+     * https://leetcode.com/problems/maximum-width-of-binary-tree/solution/
+     */
+    int ans;
+    Map<Integer, Integer> left;
+    public int widthOfBinaryTree3(TreeNode root) {
+        ans = 0;
+        left = new HashMap();
+        dfs(root, 0, 0);
+        return ans;
+    }
+    public void dfs(TreeNode root, int depth, int pos) {
+        if (root == null) return;
+        left.computeIfAbsent(depth, x-> pos);
+        ans = Math.max(ans, pos - left.get(depth) + 1);
+        dfs(root.left, depth + 1, 2 * pos);
+        dfs(root.right, depth + 1, 2 * pos + 1);
+    }
 
 }
