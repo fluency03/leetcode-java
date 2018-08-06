e281f558c6f5759af1c442361dff0bac597756d1
fluency03 <fluency.03@gmail.com>
2018-08-06 20:35:01 +0200

    CoinChangeII518

--- src/CoinChangeII518.java
+++ src/CoinChangeII518.java
@@ -38,6 +38,20 @@
 
 public class CoinChangeII518 {
     public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount + 1];
+        dp[0][0] = 1;
+        for (int i=1; i<=coins.length; i++) {
+            dp[i][0] = 1;
+            int c = coins[i-1];
+            for (int j=1; j<=amount; j++) {
+                dp[i][j] = dp[i-1][j] + (j < c ? 0 : dp[i][j-c]);
+            }
+        }
+        return dp[coins.length][amount];
+    }
+
+
+    public int change2(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
         dp[0] = 1;
         for (int coin : coins) {
