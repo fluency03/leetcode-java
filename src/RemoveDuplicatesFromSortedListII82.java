d2f2b9a77c33969d028d7625f1735a94a209e3c6
fluency03 <fluency.03@gmail.com>
2018-08-09 22:04:45 +0200

    RemoveDuplicatesFromSortedListII82

--- src/RemoveDuplicatesFromSortedListII82.java
+++ src/RemoveDuplicatesFromSortedListII82.java
@@ -97,17 +97,16 @@ public class RemoveDuplicatesFromSortedListII82 {
         ListNode dummy = new ListNode(0), fast = head, slow = dummy;
         slow.next = fast;
         while(fast != null) {
-        	while (fast.next != null && fast.val == fast.next.val) {
-         		fast = fast.next;    //while loop to find the last node of the dups.
-        	}
-        	if (slow.next != fast) { //duplicates detected.
-        		slow.next = fast.next; //remove the dups.
-        		fast = slow.next;     //reposition the fast pointer.
-        	} else { //no dup, move down both pointer.
-        		slow = slow.next;
-        		fast = fast.next;
-        	}
-
+            while (fast.next != null && fast.val == fast.next.val) {
+                fast = fast.next;    //while loop to find the last node of the dups.
+            }
+            if (slow.next != fast) { //duplicates detected.
+                slow.next = fast.next; //remove the dups.
+                fast = slow.next;     //reposition the fast pointer.
+            } else { //no dup, move down both pointer.
+                slow = slow.next;
+                fast = fast.next;
+            }
         }
         return dummy.next;
     }
