206e2c2922c41f0896c2968b185dbcef4a7d7a3d
fluency03 <fluency.03@gmail.com>
2018-08-15 21:39:44 +0200

    MultipleKnapsack

---
+++ src/knapsack/MultipleKnapsack.java
@@ -0,0 +1,34 @@
+/**
+ * There are N objects and a bag with capacity of V.
+ * The ith object is having costs[i] and weights[i].
+ * The ith object can be used with n[i] times.
+ * Find out which objects can be put into the bag in order to have maximum value.
+ */
+
+public class MultipleKnapsack {
+
+    /**
+     * dp[][] = dp[][]
+     */
+    public int maxValue(int[] costs, int[] weights, int[] limits, int V) {
+
+
+
+      
+    }
+  
+
+
+    // procedure MultiplePack(cost,weight,amount)
+    // if cost*amount>=V
+    //     CompletePack(cost,weight)
+    //     return
+    // integer k=1
+    // while k<amount
+    //     ZeroOnePack(k*cost,k*weight)
+    //     amount=amount-k
+    //     k=k*2
+    // ZeroOnePack(amount*cost,amount*weight)
+
+
+}
