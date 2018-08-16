4cffba021548cbc91a28ac4078b1207aded30a4b
fluency03 <fluency.03@gmail.com>
2018-08-16 22:22:05 +0200

    CombinationSumIV377

--- src/CombinationSumIV377.java
+++ src/CombinationSumIV377.java
@@ -74,16 +74,13 @@ public class CombinationSumIV377 {
     }
 
 
-    /**
-     * https://leetcode.com/problems/combination-sum-iv/discuss/85036/1ms-Java-DP-Solution-with-Detailed-Explanation
-     */
     public int combinationSum43(int[] nums, int target) {
         int[] comb = new int[target + 1];
         comb[0] = 1;
-        for (int i = 1; i < comb.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i - nums[j] >= 0) {
-                    comb[i] += comb[i - nums[j]];
+        for (int i = 1; i <= target; i++) {
+            for (int n: nums) {
+                if (i - n >= 0) {
+                    comb[i] += comb[i - n];
                 }
             }
         }
