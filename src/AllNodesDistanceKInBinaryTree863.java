1eaf4e51d7963fb3803cfb4e637445e0299662cd
fluency03 <fluency.03@gmail.com>
2018-08-17 09:36:36 +0200

    AllNodesDistanceKInBinaryTree863

--- src/AllNodesDistanceKInBinaryTree863.java
+++ src/AllNodesDistanceKInBinaryTree863.java
@@ -198,4 +198,40 @@ public class AllNodesDistanceKInBinaryTree863 {
     }
 
 
+    Map<TreeNode, Integer> map = new HashMap<>();
+    public List<Integer> distanceK4(TreeNode root, TreeNode target, int K) {
+        List<Integer> res = new LinkedList<>();
+        find(root, target, K);
+        dfs(root, target, K, map.get(root), res);
+        return res;
+    }
+
+    // find target node first and store the distance in that path that we could use it later directly
+    private int find(TreeNode root, TreeNode target, int K) {
+        if (root == null) return -1;
+        if (root == target) {
+            map.put(root, 0);
+            return 0;
+        }
+        int left = find(root.left, target, K);
+        if (left >= 0) {
+            map.put(root, left + 1);
+            return left + 1;
+        }
+        int right = find(root.right, target, K);
+        if (right >= 0) {
+            map.put(root, right + 1);
+            return right + 1;
+        }
+        return -1;
+    }
+
+    private void dfs(TreeNode root, TreeNode target, int K, int length, List<Integer> res) {
+        if (root == null) return;
+        if (map.containsKey(root)) length = map.get(root);
+        if (length == K) res.add(root.val);
+        dfs(root.left, target, K, length + 1, res);
+        dfs(root.right, target, K, length + 1, res);
+    }
+
 }
