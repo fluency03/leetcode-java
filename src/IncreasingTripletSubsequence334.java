d7afae8edefc4a44031ac993d9ab5e2f5f0da082
fluency03 <fluency.03@gmail.com>
2018-08-18 23:22:42 +0200

    IncreasingTripletSubsequence334

--- src/IncreasingTripletSubsequence334.java
+++ src/IncreasingTripletSubsequence334.java
@@ -19,7 +19,6 @@
 public class IncreasingTripletSubsequence334 {
     public boolean increasingTriplet(int[] nums) {
         if (nums == null || nums.length < 3) return false;
-
         int a = nums[0];
         int b = Integer.MAX_VALUE;
         for (int i=1; i<nums.length; i++) {
@@ -27,7 +26,43 @@ public class IncreasingTripletSubsequence334 {
             else if (nums[i] > a) b = nums[i];
             else a = nums[i];
         }
+        return false;
+    }
 
+    public boolean increasingTriplet2(int[] nums) {
+        if (nums == null || nums.length < 3) return false;
+        int N = nums.length;
+        int[] dp = new int[3];
+        
+        int size = 0;
+        for (int i=0; i<N; i++) {
+            int curr = nums[i];
+            int insertPoist = 0;
+            while (insertPoist < size && dp[insertPoist] < curr) {
+                insertPoist++;
+            }
+            dp[insertPoist] = curr;
+            if (insertPoist == size) size++;
+            if (size == 3) return true;
+        }
+        return false;
+    }
+
+
+    public boolean increasingTriplet3(int[] nums) {
+        if (nums == null || nums.length < 3) return false;
+        int N = nums.length;
+        int[] dp = new int[N + 1];
+        for (int i=1; i<=N; i++) {
+            int tmp = 1;
+            for (int j=1; j<i; j++) {
+                if (nums[i-1] > nums[j-1] && dp[j] + 1 > tmp) {
+                    tmp = dp[j] + 1;
+                }
+                if (tmp >= 3) return true;
+            }
+            dp[i] = tmp;
+        }
         return false;
     }
 
