c8c29afb760bba0fbbaebfda2f9555df356adfce
fluency03 <fluency.03@gmail.com>
2018-08-25 20:46:31 +0200

    LoggerRateLimiter359

--- src/LoggerRateLimiter359.java
+++ src/LoggerRateLimiter359.java
@@ -53,6 +53,36 @@ public class LoggerRateLimiter359 {
         }
     }
 
+    /**
+     * https://leetcode.com/problems/logger-rate-limiter/discuss/83254/Java-with-a-LinkedHashMap-and-using-removeEldestEntry
+     */
+    class Logger2 {
+        public Map<String, Integer> map;
+        int lastSecond = 0;
+
+        /** Initialize your data structure here. */
+        public Logger() {
+            map = new LinkedHashMap<String, Integer>(100, 0.6f, true) {
+                protected boolean removeEldestEntry(Map.Entry<String, Integer> eldest) {
+                    return lastSecond - eldest.getValue() > 10;
+                }
+            };
+        }
+
+        /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+            If this method returns false, the message will not be printed.
+            The timestamp is in seconds granularity. */
+        public boolean shouldPrintMessage(int timestamp, String message) {
+            lastSecond = timestamp;
+            if(!map.containsKey(message)||timestamp - map.get(message) >= 10){
+                map.put(message,timestamp);
+                return true;
+            }
+            return false;
+        }
+    }
+
+
 /**
  * Your Logger object will be instantiated and called as such:
  * Logger obj = new Logger();
