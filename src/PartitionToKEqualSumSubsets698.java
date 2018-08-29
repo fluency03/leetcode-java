ddcf5ba51daf8b215ffcbd6d268e3a5131ad0ea7
fluency03 <fluency.03@gmail.com>
2018-08-29 21:12:01 +0200

    PartitionToKEqualSumSubsets698

--- src/PartitionToKEqualSumSubsets698.java
+++ src/PartitionToKEqualSumSubsets698.java
@@ -71,4 +71,37 @@ public class PartitionToKEqualSumSubsets698 {
         return dp[(1 << N) - 1];
     }
 
+
+    /**
+     * https://leetcode.com/problems/partition-to-k-equal-sum-subsets/solution/
+     */
+    public boolean search(int[] groups, int row, int[] nums, int target) {
+        if (row < 0) return true;
+        int v = nums[row--];
+        for (int i = 0; i < groups.length; i++) {
+            if (groups[i] + v <= target) {
+                groups[i] += v;
+                if (search(groups, row, nums, target)) return true;
+                groups[i] -= v;
+            }
+            if (groups[i] == 0) break;
+        }
+        return false;
+    }
+
+    public boolean canPartitionKSubsets3(int[] nums, int k) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % k > 0) return false;
+        int target = sum / k;
+
+        Arrays.sort(nums);
+        int row = nums.length - 1;
+        if (nums[row] > target) return false;
+        while (row >= 0 && nums[row] == target) {
+            row--;
+            k--;
+        }
+        return search(new int[k], row, nums, target);
+    }
+
 }
