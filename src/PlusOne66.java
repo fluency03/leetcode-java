e918fb7fa553bb3ae6a1a7ed1d92fa978c5bd465
fluency03 <fluency.03@gmail.com>
2018-09-04 23:19:41 +0200

    PlusOne66

--- src/PlusOne66.java
+++ src/PlusOne66.java
@@ -16,7 +16,7 @@ public class PlusOne66 {
         for (int i=digits.length-1; i>=0; i--) {
             int s = digits[i] + c;
             c = s / 10;
-            res[i+1] = s%10;
+            res[i+1] = s % 10;
         }
         res[0] = c;
         return (res[0] != 0) ? res : Arrays.copyOfRange(res, 1, res.length);
@@ -27,20 +27,16 @@ public class PlusOne66 {
      * https://leetcode.com/problems/plus-one/discuss/24082/My-Simple-Java-Solution
      */
     public int[] plusOne2(int[] digits) {
-
         int n = digits.length;
         for(int i=n-1; i>=0; i--) {
             if(digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
-
             digits[i] = 0;
         }
-
         int[] newNumber = new int [n+1];
         newNumber[0] = 1;
-
         return newNumber;
     }
 
