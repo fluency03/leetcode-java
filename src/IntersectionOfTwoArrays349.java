3a46d012bea690363de71e08e2acd94ad8efadaa
fluency03 <fluency.03@gmail.com>
2018-09-05 22:43:50 +0200

    IntersectionOfTwoArrays349

--- src/IntersectionOfTwoArrays349.java
+++ src/IntersectionOfTwoArrays349.java
@@ -40,16 +40,32 @@ public class IntersectionOfTwoArrays349 {
     }
 
 
-    public static void main(String[] args) {
-        
-        int[] nums1 = new int[]{1, 2, 2, 1};
-        int[] nums2 = new int[]{2, 2};
-
-        int[] res = intersection(nums1, nums2);
-        for (int i: res) {
-            System.out.println(i);
+    /**
+     * https://leetcode.com/problems/intersection-of-two-arrays/discuss/81969/Three-Java-Solutions
+     */
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
         }
-        
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
     }
 
 }
