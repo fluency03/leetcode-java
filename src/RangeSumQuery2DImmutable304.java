76085a7679e4b82359b8b69f2f3682baf48c3368
fluency03 <fluency.03@gmail.com>
2018-09-20 18:59:57 +0200

    RangeSumQuery2DImmutable304

--- src/RangeSumQuery2DImmutable304.java
+++ src/RangeSumQuery2DImmutable304.java
@@ -30,7 +30,6 @@
 
 
 public class RangeSumQuery2DImmutable304 {
-
     class NumMatrix {
         private int[][] sum;
         public NumMatrix(int[][] matrix) {
@@ -50,6 +49,42 @@ public class RangeSumQuery2DImmutable304 {
             return sum[row2+1][col2+1] - sum[row2+1][col1] - sum[row1][col2+1] + sum[row1][col1];
         }
     }
+
+
+    class NumMatrix2 {
+        private int[][] sums;
+        private int M;
+        private int N;
+
+        public NumMatrix2(int[][] matrix) {
+            if (matrix.length != 0 && matrix[0].length != 0) {
+                this.M = matrix.length;
+                this.N = matrix[N].length;
+                this.sums = new int[M][N + 1];
+                for (int i=0; i<M; i++) {
+                    for (int j=1; j<=N; j++) {
+                        this.sums[i][j] += this.sums[i][j-1] + matrix[i][j-1];
+                    }
+                }
+            }
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            int res = 0;
+            for (int i=row1; i<=row2; i++) {
+                res += this.sums[i][col2+1] - this.sums[i][col1];
+            }
+            return res;
+        }
+    }
+    
+    /**
+     * Your NumMatrix object will be instantiated and called as such:
+     * NumMatrix obj = new NumMatrix(matrix);
+     * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+     */
+
+
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
