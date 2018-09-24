8e057fc086f7d90d535d919cdd02082aa5634a88
fluency03 <fluency.03@gmail.com>
2018-09-24 08:21:53 +0200

    EditDistance72

--- src/EditDistance72.java
+++ src/EditDistance72.java
@@ -16,13 +16,9 @@ public class EditDistance72 {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-
         int[][] d = new int[m + 1][n + 1];
-
-        for (int i = 0; i <= m; i++)
-            d[i][0] = i;
-        for (int i = 1; i <= n; i++)
-            d[0][i] = i;
+        for (int i = 0; i <= m; i++) d[i][0] = i;
+        for (int i = 1; i <= n; i++) d[0][i] = i;
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
