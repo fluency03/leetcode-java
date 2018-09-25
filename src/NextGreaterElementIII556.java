a130c15bc0e0ea214870c93da9b3b094d474a1bf
fluency03 <fluency.03@gmail.com>
2018-09-25 22:34:16 +0200

    NextGreaterElementIII556

--- src/NextGreaterElementIII556.java
+++ src/NextGreaterElementIII556.java
@@ -53,4 +53,38 @@ public class NextGreaterElementIII556 {
         digits[j] = temp;
     }
 
+    public int nextGreaterElement2(int n) {
+        int[] digits = new int[10];
+        int k = n;
+        int i = 9;
+        while (k != 0) {
+            digits[i--] = k % 10;
+            k /= 10;
+        }
+        if (i == 8) return -1;
+
+        int j = 8;
+        while (j > i) {
+            if (digits[j] < digits[j+1]) break;
+            j--;
+        }
+        if (j == i) return -1;
+        
+        int p = 9;
+        while (p > j) {
+            if (digits[p] > digits[j]) break;
+            p--;
+        }
+        swap(digits, j, p);
+        Arrays.sort(digits, j+1, 10);
+        
+        long res = 0;
+        long level = 1;
+        for (int q=9; q>=0; q--) {
+            res += digits[q] * level;
+            level *= 10;
+        }
+        return res > Integer.MAX_VALUE ? -1 : (int) res;
+    }
+
 }
