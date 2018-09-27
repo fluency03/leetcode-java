f5b55236617a069534f78840d0e6d9ed744078ac
fluency03 <fluency.03@gmail.com>
2018-09-27 21:23:34 +0200

    Subsequence

--- src/Subsequence.java
+++ src/Subsequence.java
@@ -16,21 +16,6 @@ public class Subsequence {
         return res;
     }
 
-    // public boolean isInsertable(String target, String word) {
-    //     int M = target.length();
-    //     int N = word.length();
-    //     char[] charsT = target.toCharArray();
-    //     char[] charsW = word.toCharArray();
-    //     boolean[][] dp = new boolean[M+1][N+1];
-    //     for (int i=0; i<=N; i++) dp[0][i] = true;
-    //     for (int i=1; i<=M; i++) {
-    //         for (int j=i; j<=N; j++) {
-    //             dp[i][j] = (dp[i-1][j-1] && charsT[i-1] == charsW[j-1]) || (dp[i][j-1]);
-    //         }
-    //     }
-    //     return dp[M][N];
-    // }
-
     public boolean isSubsequence(String s, String t) {
         int i = 0;
         int j = 0;
