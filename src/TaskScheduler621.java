c0bd511810a44a239fd4a1d151ec3b5a2e7b4454
fluency03 <fluency.03@gmail.com>
2018-09-29 07:51:36 +0200

    TaskScheduler621

--- src/TaskScheduler621.java
+++ src/TaskScheduler621.java
@@ -106,4 +106,31 @@ public class TaskScheduler621 {
         return Math.max((n+1) * (max-1) + maxCount, N);
     }
 
+
+    /**
+     * https://leetcode.com/problems/task-scheduler/discuss/104500/Java-O(n)-time-O(1)-space-1-pass-no-sorting-solution-with-detailed-explanation
+     */
+    public int leastInterval5(char[] tasks, int n) {
+        int[] counter = new int[26];
+        int max = 0;
+        int maxCount = 0;
+        for(char task : tasks) {
+            counter[task - 'A']++;
+            if(max == counter[task - 'A']) {
+                maxCount++;
+            }
+            else if(max < counter[task - 'A']) {
+                max = counter[task - 'A'];
+                maxCount = 1;
+            }
+        }
+        int partCount = max - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.length - max * maxCount;
+        int idles = Math.max(0, emptySlots - availableTasks);
+        
+        return tasks.length + idles;
+    }
+
 }
