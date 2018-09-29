2f235e041cdb0773a84d058d8917699589f75b7d
fluency03 <fluency.03@gmail.com>
2018-09-29 09:02:02 +0200

    ReorganizeString767

--- src/ReorganizeString767.java
+++ src/ReorganizeString767.java
@@ -127,4 +127,41 @@ public class ReorganizeString767 {
         return idx;
     }
 
+
+    /**
+     * https://leetcode.com/problems/reorganize-string/discuss/113451/7-ms-Java-O(n)-Solution.-no-Sorting
+     */
+    public String reorganizeString4(String S) {
+        int n = S.length();
+        int[] cnt = new int[128];
+        char mc = 'a';
+        for (char c : S.toCharArray()) {
+            cnt[c]++;
+            mc = (cnt[c] > cnt[mc]) ? c : mc;
+        }
+        if (cnt[mc] == 1) {
+            return S;
+        }
+        if (n - cnt[mc] <= cnt[mc] - 2) {
+            return "";
+        }
+        StringBuilder[] sb = new StringBuilder[cnt[mc]];
+        for (int i = 0; i < sb.length; i ++) {
+            sb[i] = new StringBuilder();
+            sb[i].append(mc);
+        }
+        int k = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (c != mc && cnt[c] > 0) {
+                sb[k++].append(c);
+                cnt[c]--;
+                k %= sb.length;
+            }
+        }
+        for (int i = 1; i < sb.length; i++) {
+            sb[0].append(sb[i]);
+        }
+        return sb[0].toString();
+    }
+
 }
