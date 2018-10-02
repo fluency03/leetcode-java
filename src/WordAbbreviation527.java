08607e520a842cdcd13ecd010c30b1bfc24b353e
fluency03 <fluency.03@gmail.com>
2018-10-02 18:30:07 +0200

    WordAbbreviation527

--- src/WordAbbreviation527.java
+++ src/WordAbbreviation527.java
@@ -73,4 +73,114 @@ public class WordAbbreviation527 {
         } 
     }
 
+
+    /**
+     * https://leetcode.com/problems/word-abbreviation/solution/s
+     */
+    public List<String> wordsAbbreviation2(List<String> words) {
+        Map<String, List<IndexedWord>> groups = new HashMap();
+        String[] ans = new String[words.size()];
+
+        int index = 0;
+        for (String word: words) {
+            String ab = abbrev(word, 0);
+            if (!groups.containsKey(ab))
+                groups.put(ab, new ArrayList());
+            groups.get(ab).add(new IndexedWord(word, index));
+            index++;
+        }
+
+        for (List<IndexedWord> group: groups.values()) {
+            Collections.sort(group, (a, b) -> a.word.compareTo(b.word));
+
+            int[] lcp = new int[group.size()];
+            for (int i = 1; i < group.size(); ++i) {
+                int p = longestCommonPrefix(group.get(i-1).word, group.get(i).word);
+                lcp[i] = p;
+                lcp[i-1] = Math.max(lcp[i-1], p);
+            }
+
+            for (int i = 0; i < group.size(); ++i)
+                ans[group.get(i).index] = abbrev(group.get(i).word, lcp[i]);
+        }
+
+        return Arrays.asList(ans);
+    }
+
+    public String abbrev(String word, int i) {
+        int N = word.length();
+        if (N - i <= 3) return word;
+        return word.substring(0, i+1) + (N - i - 2) + word.charAt(N-1);
+    }
+
+    public int longestCommonPrefix(String word1, String word2) {
+        int i = 0;
+        while (i < word1.length() && i < word2.length()
+                && word1.charAt(i) == word2.charAt(i))
+            i++;
+        return i;
+    }
+
+    class IndexedWord {
+        String word;
+        int index;
+        IndexedWord(String w, int i) {
+            word = w;
+            index = i;
+        }
+    }
+
+
+    /**
+     * https://leetcode.com/problems/word-abbreviation/solution/s
+     */
+    public List<String> wordsAbbreviation3(List<String> words) {
+        Map<String, List<IndexedWord>> groups = new HashMap();
+        String[] ans = new String[words.size()];
+
+        int index = 0;
+        for (String word: words) {
+            String ab = abbrev(word, 0);
+            if (!groups.containsKey(ab))
+                groups.put(ab, new ArrayList());
+            groups.get(ab).add(new IndexedWord(word, index));
+            index++;
+        }
+
+        for (List<IndexedWord> group: groups.values()) {
+            TrieNode trie = new TrieNode();
+            for (IndexedWord iw: group) {
+                TrieNode cur = trie;
+                for (char letter: iw.word.substring(1).toCharArray()) {
+                    if (cur.children[letter - 'a'] == null)
+                        cur.children[letter - 'a'] = new TrieNode();
+                    cur.count++;
+                    cur = cur.children[letter - 'a'];
+                }
+            }
+
+            for (IndexedWord iw: group) {
+                TrieNode cur = trie;
+                int i = 1;
+                for (char letter: iw.word.substring(1).toCharArray()) {
+                    if (cur.count == 1) break;
+                    cur = cur.children[letter - 'a'];
+                    i++;
+                }
+                ans[iw.index] = abbrev(iw.word, i-1);
+            }
+        }
+
+        return Arrays.asList(ans);
+    }
+
+    class TrieNode {
+        TrieNode[] children;
+        int count;
+        TrieNode() {
+            children = new TrieNode[26];
+            count = 0;
+        }
+    }
+
 }
