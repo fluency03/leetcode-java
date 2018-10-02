ae090e22952d30bf62fd62949e3630119ed6cbf1
fluency03 <fluency.03@gmail.com>
2018-10-02 18:59:49 +0200

    GeneralizedAbbreviation320

--- src/GeneralizedAbbreviation320.java
+++ src/GeneralizedAbbreviation320.java
@@ -33,13 +33,42 @@ public class GeneralizedAbbreviation320 {
         if (prevIsWord) {
             for (int j=i; j<word.length; j++) {
                 int num = j-i+1;
-                String numString = Integer.toString(num);
-                sb.append(numString);
+                int len = sb.length();
+                sb.append(num);
                 backtrace(word, res, false, j+1, sb);
-                sb.delete(sb.length()-numString.length(), sb.length());
+                sb.setLength(len);
             }
         }
     }
 
+
+    /**
+     * https://leetcode.com/problems/generalized-abbreviation/solution/
+     */
+    public List<String> generateAbbreviations2(String word) {
+        List<String> ans = new ArrayList<>();
+        for (int x = 0; x < (1 << word.length()); ++x) // loop through all possible x
+            ans.add(abbr(word, x));
+        return ans;
+    }
+
+    // build the abbreviation for word from number x
+    private String abbr(String word, int x) {
+        StringBuilder builder = new StringBuilder();
+        int k = 0, n = word.length(); // k is the count of consecutive ones in x
+        for (int i = 0; i < n; ++i, x >>= 1) {
+            if ((x & 1) == 0) { // bit is zero, we keep word.charAt(i)
+                if (k != 0) { // we have abbreviated k characters
+                    builder.append(k);
+                    k = 0; // reset the counter k
+                }
+                builder.append(word.charAt(i));
+            }
+            else // bit is one, increase k
+                ++k;
+        }
+        if (k != 0) builder.append(k); //don't forget to append the last k if non zero
+        return builder.toString();
+    }
 
 }
