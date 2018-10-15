57e9130776cbb235dc104ee824d45bd3b543c436
fluency03 <fluency.03@gmail.com>
2018-10-15 22:21:28 +0200

    solveEquation2

--- src/SolveTheEquation640.java
+++ src/SolveTheEquation640.java
@@ -109,4 +109,30 @@ public class SolveTheEquation640 {
         }
     }
 
+
+    /**
+     * https://leetcode.com/problems/solve-the-equation/discuss/105311/Concise-Java-Solution
+     */
+    public String solveEquation2(String equation) {
+        int[] res = evaluateExpression(equation.split("=")[0]);
+        int[] res2 = evaluateExpression(equation.split("=")[1]);
+        res[0] -= res2[0];
+        res[1] = res2[1] - res[1];
+        if (res[0] == 0 && res[1] == 0) return "Infinite solutions";
+        if (res[0] == 0) return "No solution";
+        return "x=" + res[1]/res[0];
+    }  
+
+    public int[] evaluateExpression(String exp) {
+        String[] tokens = exp.split("(?=[-+])"); 
+        int[] res =  new int[2];
+        for (String token : tokens) {
+            if (token.equals("+x") || token.equals("x")) res[0] += 1;
+            else if (token.equals("-x")) res[0] -= 1;
+            else if (token.contains("x")) res[0] += Integer.parseInt(token.substring(0, token.indexOf("x")));
+            else res[1] += Integer.parseInt(token);
+        }
+        return res;
+    }
+
 }
