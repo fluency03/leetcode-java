0b34d29ae4c7e3db85a9eb412ef35e37430e6178
fluency03 <fluency.03@gmail.com>
2018-10-15 22:13:36 +0200

    calculate4

--- src/BasicCalculator224.java
+++ src/BasicCalculator224.java
@@ -218,4 +218,32 @@ public class BasicCalculator224 {
         return j;
     }
 
+    /**
+     * https://leetcode.com/problems/basic-calculator-ii/discuss/63088/Explanation-for-Java-O(n)-time-and-O(1)-space-solution
+     */
+    public int calculate4(String s) {
+        int pre = 0, curr = 0, sign = 1, op = 0, num = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + (s.charAt(i) - '0');
+                if (i == s.length() - 1 || !Character.isDigit(s.charAt(i + 1))) {
+                    curr = (op == 0 ? num : (op == 1 ? curr * num : curr / num));
+                }
+                
+            } else if (s.charAt(i) == '*' || s.charAt(i) == '/') {
+                op = (s.charAt(i) == '*' ? 1 : -1);
+                num = 0;
+                
+            } else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+                pre += sign * curr;
+                sign = (s.charAt(i) == '+' ? 1 : -1);
+                op = 0;
+                num = 0;
+            }
+        }
+        
+        return pre + sign * curr;
+    }
+
 }
