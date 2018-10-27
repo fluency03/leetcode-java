163808cf6de5adccd333deb139b7092c0e622212
fluency03 <fluency.03@gmail.com>
2018-10-27 22:41:46 +0100

    CheapestFlightsWithinKStops787

--- src/CheapestFlightsWithinKStops787.java
+++ src/CheapestFlightsWithinKStops787.java
@@ -78,5 +78,58 @@ public class CheapestFlightsWithinKStops787 {
         return res;
     }
 
+
+    /**
+     * https://leetcode.com/problems/cheapest-flights-within-k-stops/solution/
+     */
+    public int findCheapestPrice2(int n, int[][] flights, int src, int dst, int K) {
+        int[][] dist = new int[2][n];
+        int INF = Integer.MAX_VALUE / 2;
+        Arrays.fill(dist[0], INF);
+        Arrays.fill(dist[1], INF);
+        dist[0][src] = dist[1][src] = 0;
+
+        for (int i = 0; i <= K; ++i)
+            for (int[] edge: flights)
+                dist[i&1][edge[1]] = Math.min(dist[i&1][edge[1]], dist[~i&1][edge[0]] + edge[2]);
+
+        return dist[K&1][dst] < INF ? dist[K&1][dst] : -1;
+    }
+
+
+    /**
+     * https://leetcode.com/problems/cheapest-flights-within-k-stops/solution/
+     */
+    public int findCheapestPrice3(int n, int[][] flights, int src, int dst, int K) {
+        int[][] graph = new int[n][n];
+        for (int[] flight: flights)
+            graph[flight[0]][flight[1]] = flight[2];
+
+        Map<Integer, Integer> best = new HashMap();
+
+        Comparator<int[]> comp = (a, b) -> a[0] - b[0];
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(comp);
+        pq.offer(new int[]{0, 0, src});
+
+        while (!pq.isEmpty()) {
+            int[] info = pq.poll();
+            int cost = info[0], k = info[1], place = info[2];
+            if (k > K+1 || cost > best.getOrDefault(k * 1000 + place, Integer.MAX_VALUE))
+                continue;
+            if (place == dst)
+                return cost;
+
+            for (int nei = 0; nei < n; ++nei) if (graph[place][nei] > 0) {
+                int newcost = cost + graph[place][nei];
+                if (newcost < best.getOrDefault((k+1) * 1000 + nei, Integer.MAX_VALUE)) {
+                    pq.offer(new int[]{newcost, k+1, nei});
+                    best.put((k+1) * 1000 + nei, newcost);
+                }
+            }
+        }
+
+        return -1;
+    }
+
 }
 
